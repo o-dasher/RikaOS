5e9747184aae91082a89e829897f5a4f1a01faa2
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-05-05 21:10:17 -0300

    Proper catppuccin setup

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,14 +1,26 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
+let
+	inherit (lib) toUpper stringLength substring;
+
+	capital = s : toUpper (substring 0 1 s) + (substring 1 (stringLength s) s);
+	c = {
+		flavour = "mocha";
+		accent = "pink";
+	};
+in
 {
+	xdg.enable = true;
+	catppuccin = c;
 	gtk = {
 		enable = true;
-		iconTheme = {
-			package = pkgs.whitesur-icon-theme;
-			name = "WhiteSur-Dark";
-		};
 		theme = {
-			package = pkgs.whitesur-gtk-theme;
-			name = "WhiteSur-Dark";
-		};
-	};
+			name = "Catppuccin-${capital c.flavour}-Standard-Pink-Dark";
+			package = pkgs.catppuccin-gtk.override {
+			  accents = [c.accent];
+			  size = "standard";
+			  tweaks = [];
+			  variant = c.flavour;
+			};
+      };
+  };
 }
