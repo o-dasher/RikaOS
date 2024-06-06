40926973f649563b24c05f412902b8b996957c93
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-06 20:41:58 -0300

    Improve fish configuration

--- nixo/home/rika/fish/default.nix
+++ nixo/home/rika/fish/default.nix
@@ -11,21 +11,18 @@
     fish = {
       enable = true;
       catppuccin.enable = true;
+      shellAliases = {
+        hm = "home-manager switch --flake ~/.config/nixo";
+        sail = "bash vendor/bin/sail";
+      };
       interactiveShellInit = ''
         direnv hook fish | source
         starship init fish | source
-
-        if status is-interactive
-        	# Commands to run in interactive sessions can go here
-        end
 
         function fish_greeting
         	echo Welcome(set_color magenta) home(set_color normal) $USER how are you doing today\?
         	echo (set_color magenta; date)
         end
-
-        alias hm "home-manager switch --flake ~/.config/nixo"
-		alias sail "bash vendor/bin/sail"
       '';
     };
   };
