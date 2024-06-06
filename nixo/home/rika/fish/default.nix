e4697481c7a0987ebbf6a90d4be16bc6df5c89ed
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-06 20:49:21 -0300

    Adds lazygit and remove redundancy

--- nixo/home/rika/fish/default.nix
+++ nixo/home/rika/fish/default.nix
@@ -13,12 +13,10 @@
       catppuccin.enable = true;
       shellAliases = {
         hm = "home-manager switch --flake ~/.config/nixo";
+		lg = "lazygit";
         sail = "bash vendor/bin/sail";
       };
       interactiveShellInit = ''
-        direnv hook fish | source
-        starship init fish | source
-
         function fish_greeting
         	echo Welcome(set_color magenta) home(set_color normal) $USER how are you doing today\?
         	echo (set_color magenta; date)
