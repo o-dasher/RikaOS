0b5fbcc1a6f4b575c7a1272d321c898df21905c7
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-07 19:22:43 +0000

    feat(neovim): add catppuccin to lazygit + global gtk theming

--- nixo/home/rika/theme/default.nix
+++ nixo/home/rika/theme/default.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   catppuccin = {
     flavour = "mocha";
     accent = "pink";
+  };
+  gtk = {
+    enable = true;
+    catppuccin.enable = true;
+    iconTheme = {
+      name = "WhiteSur";
+      package = pkgs.whitesur-icon-theme;
+    };
   };
 }
