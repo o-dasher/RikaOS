e1828759bca5fdabaf5dd4bdd43643fa8e146aae
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-08 18:33:47 +0000

    refactor(assets): change wallpaper depending on current desktop

--- nixo/home/satoko/default.nix
+++ nixo/home/satoko/default.nix
@@ -5,38 +5,37 @@
     ./swaync
     ./waybar
     ./wofi
-	../rika
+    ../rika
   ];
 
-  config = {
-    home.packages = with pkgs; [
-      # Sys utils
-      xdg-terminal-exec
-      pamixer
+  stylix.image = ../../../assets/Wallpapers/ryo.png;
+  home.packages = with pkgs; [
+    # Sys utils
+    xdg-terminal-exec
+    pamixer
 
-      # Desktop
-      wofi
-      jq
-      grimblast
-      wl-clipboard
+    # Desktop
+    wofi
+    jq
+    grimblast
+    wl-clipboard
 
-      # General
-      brave
-      armcord
-      qbittorrent
-      obs-studio
-      stremio
-      htop
-      xfce.thunar
-      gdu
-      pavucontrol
-      hyfetch
-    ];
+    # General
+    brave
+    armcord
+    qbittorrent
+    obs-studio
+    stremio
+    htop
+    xfce.thunar
+    gdu
+    pavucontrol
+    hyfetch
+  ];
 
-    programs = {
-      mpv = {
-        enable = true;
-      };
+  programs = {
+    mpv = {
+      enable = true;
     };
   };
 }
