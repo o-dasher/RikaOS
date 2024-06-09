c86cc5e4a6b27265455a99a6f39a97fe416b6c20
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-09 18:02:17 -0300

    Move lsps to home manager

--- nixo/home/rika/default.nix
+++ nixo/home/rika/default.nix
@@ -65,9 +65,22 @@ in
       viAlias = true;
       vimAlias = true;
       extraPackages = with pkgs; [
-        tree-sitter
-        ripgrep
-        gcc
+		# LSP
+		lua-language-server
+		nixd
+		phpactor
+		yaml-language-server
+		clang-tools_16
+		pyright
+		ruff-lsp
+		nodePackages."@tailwindcss/language-server"
+		nodePackages.typescript-language-server
+		nodePackages.vscode-langservers-extracted
+
+		# Some tools
+        tree-sitter # Code highlightning
+        ripgrep # Telescope fzf
+        gcc # Installing tree-sitteer grammars
       ];
     };
     direnv = {
--- nvim/lua/thiago/lazy/lsp.lua
+++ nvim/lua/thiago/lazy/lsp.lua
@@ -1,9 +1,8 @@
 return {
 	"VonHeikemen/lsp-zero.nvim",
-	event = "BufReadPost",
 	dependencies = {
-		"dundalek/lazy-lsp.nvim",
 		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/nvim-cmp",
 	},
 	config = function()
@@ -13,34 +12,32 @@ return {
 			-- see :help lsp-zero-keybindings to learn the available actions
 			lsp_zero.default_keymaps({
 				buffer = bufnr,
-				preserve_mappings = false
+				preserve_mappings = false,
 			})
 
 			for _, set in pairs({
 				{ "vd", vim.diagnostic.open_float },
+				{ "vr", vim.lsp.buf.references },
 				{ "rn", vim.lsp.buf.rename },
-				{ "ca", vim.lsp.buf.code_action }
+				{ "ca", vim.lsp.buf.code_action },
 			}) do
-				local key, fun = unpack(set);
-				vim.keymap.set("n", "<leader>" .. key, fun, { buffer = bufnr, remap = false });
+				local key, fun = unpack(set)
+				vim.keymap.set("n", "<leader>" .. key, fun, { buffer = bufnr, remap = false })
 			end
 		end)
 
-		require("lazy-lsp").setup {
-			preferred_servers = {
-				markdown = {},
-				lua = { "lua_ls" },
-				nix = { "nixd" },
-				php = { "phpactor" },
-				yaml = { "yamlls" },
-				c = { "clangd" },
-				cpp = { "clangd" },
-				css = { "cssls", "tailwindcss" },
-				typescript = { "tsserver" },
-				typescriptreact = { "tsserver", "tailwindcss" },
-				python = { "pyright", "ruff_lsp" },
-				rust = {"rust_analyzer"}
-			},
-		}
-	end
+		lspcfg = require("lspconfig")
+
+		lspcfg.lua_ls.setup({})
+		lspcfg.nixd.setup({})
+		lspcfg.phpactor.setup({})
+		lspcfg.clangd.setup({})
+		lspcfg.pyright.setup({})
+		lspcfg.ruff_lsp.setup({})
+		lspcfg.yamlls.setup({})
+		lspcfg.tailwindcss.setup({})
+		lspcfg.cssls.setup({})
+		lspcfg.rust_analyzer.setup({})
+		lspcfg.tsserver.setup({})
+	end,
 }
