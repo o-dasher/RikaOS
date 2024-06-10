8c2379f0c4e2900c7dbae3d7ce7cf74d5415e59f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-10 11:28:53 -0400

    Fix typo

--- nvim/lua/thiago/lazy/lsp.lua
+++ nvim/lua/thiago/lazy/lsp.lua
@@ -35,7 +35,7 @@ return {
 			init_options = {
 				["language_server.diagnostics_on_update"] = false,
 				["language_server.diagnostics_on_open"] = false,
-				["lanaguge_server.diagnostics_on_save"] = false,
+				["language_server.diagnostics_on_save"] = false,
 				["language_server_phpstan.enabled"] = false,
 				["language_server_psalm.enabled"] = false,
 			},
