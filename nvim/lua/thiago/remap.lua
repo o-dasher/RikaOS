475dea776fa72975f4981db79968a36e0dea9931
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-09 21:43:44 -0300

    Adds remaps for copying to system clipboard on neovim

--- nvim/lua/thiago/remap.lua
+++ nvim/lua/thiago/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
+
+-- Clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
