1ca18fc2bc7ce94b15d9851492f9623267390c67
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-10 10:32:52 -0400

    Move nixGL customizations to its proper module

--- nixo/home/hanyuu/default.nix
+++ nixo/home/hanyuu/default.nix
@@ -1,5 +1,20 @@
+{ inputs, pkgs, ... }:
+let
+  nixGLIntel = inputs.nixGL.packages.${pkgs.system}.nixGLIntel;
+in
 {
-  imports = [ ../rika ];
+  imports = [
+    ../rika
+    (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
+      sha256 = "74f9fb98f22581eaca2e3c518a0a3d6198249fb1490ab4a08f33ec47827e85db";
+    })
+  ];
+
   targets.genericLinux.enable = true;
   stylix.image = ../../../assets/Wallpapers/purplyu.jpg;
+
+  # Fix opengl on nox nixos system. Keep an eye on https://github.com/nix-community/home-manager/issues/3968
+  nixGL.prefix = "${nixGLIntel}/bin/nixGLIntel";
+  home.packages = [ nixGLIntel ];
 }
