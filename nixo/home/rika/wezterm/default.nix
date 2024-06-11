d906a699a01425a8ea0b869da1d95bfb7301fb25
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-11 00:50:02 +0000

    refactor: move nixgl wraps to hanyuu

--- nixo/home/rika/wezterm/default.nix
+++ nixo/home/rika/wezterm/default.nix
@@ -1,8 +1,6 @@
-{ config, pkgs, ... }:
 {
   programs.wezterm = {
     enable = true;
-	package = (config.lib.nixGL.wrap pkgs.wezterm);
     extraConfig = ''
         local wezterm = require 'wezterm'
         local config = {}
