9e87f212904282b5a8dfc4406476ef53e57fcf97
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-14 18:05:56 -0300

    Remove useless session variables, home-manager already handles xdg

--- nixo/system/configuration.nix
+++ nixo/system/configuration.nix
@@ -96,14 +96,6 @@ in
   };
 
   environment = {
-    sessionVariables = {
-      XDG_CACHE_HOME = "\$HOME/.cache";
-      XDG_CONFIG_HOME = "\$HOME/.config";
-      XDG_BIN_HOME = "\$HOME/.local/bin";
-      XDG_DATA_HOME = "\$HOME/.local/share";
-      SYSTEM_SHARE = "/run/current-system/sw/share/";
-    };
-
     # This should be kept to a minimal. Don't ask me why, I think it is better this way.
     systemPackages = with pkgs; [
       catppuccin-sddm-corners
