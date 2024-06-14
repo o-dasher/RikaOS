72b649c4b592ca86754030b7b6a65a0779477592
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-14 23:23:03 +0000

    feat(neovim): add moving lines around like emacs

--- nvim/lua/thiago/remap.lua
+++ nvim/lua/thiago/remap.lua
@@ -1,9 +1,14 @@
 vim.g.mapleader = " "
 
+-- File
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- Easy delete lines
+
+-- Move lines around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
