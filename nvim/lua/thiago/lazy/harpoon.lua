e4b7c16575cf7c27bf18169cdacbedd901f895c9
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-15 00:09:32 +0000

    refactor(neovim): change harpoon to use normal mode by default

--- nvim/lua/thiago/lazy/harpoon.lua
+++ nvim/lua/thiago/lazy/harpoon.lua
@@ -25,21 +25,26 @@ local keys = {
 
 			local telescope_cfg = require("telescope.config")
 
+			local function deleteCursorEntry(buffer)
+				local state = require("telescope.actions.state")
+
+				local selected_entry = state.get_selected_entry()
+				local current_picker = state.get_current_picker(buffer)
+
+				table.remove(h():list().items, selected_entry.index)
+				current_picker:refresh(finder())
+			end
+
 			require("telescope.pickers")
 				.new({}, {
-					prompt_title = "Harpoon",
+					prompt_title = "harpoon",
 					finder = finder(),
+					initial_mode = "normal",
 					previewer = telescope_cfg.values.file_previewer({}),
 					sorter = telescope_cfg.values.generic_sorter({}),
-					attach_mappings = function(prompt_bufnr, map)
-						map("i", "<C-d>", function()
-							local state = require("telescope.actions.state")
-
-							local selected_entry = state.get_selected_entry()
-							local current_picker = state.get_current_picker(prompt_bufnr)
-
-							table.remove(h():list().items, selected_entry.index)
-							current_picker:refresh(finder())
+					attach_mappings = function(buffer, map)
+						map("n", "<c-d>", function()
+							deleteCursorEntry(buffer)
 						end)
 						return true
 					end,
@@ -59,7 +64,7 @@ for i = 1, 5, 1 do
 end
 
 return {
-	"ThePrimeagen/harpoon",
+	"theprimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = keys,
