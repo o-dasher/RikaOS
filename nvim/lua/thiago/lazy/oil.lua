f6538267c02bbef4b9f5e927cbe0ad4e5b6d947c
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-15 22:51:55 +0000

    feat(neovim): add oil.nvim

---
+++ nvim/lua/thiago/lazy/oil.lua
@@ -0,0 +1,17 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"pv",
+			"<CMD>Oil<CR>",
+		},
+	},
+	opts = {
+		default_file_explorer = true,
+		columns = { "icon" },
+		view_options = {
+			show_hidden = true,
+		},
+	},
+}
