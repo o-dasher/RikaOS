10940deb9bf7f655787b58fa9d08e4cbedacf4df
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-16 00:18:58 -0300

    Modularize hanyuu

---
+++ nixo/home/hanyuu/nixgl/default.nix
@@ -0,0 +1,23 @@
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}:
+let
+  nixGLIntel = inputs.nixGL.packages.${pkgs.system}.nixGLIntel;
+in
+{
+  imports = [
+    (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
+      sha256 = "74f9fb98f22581eaca2e3c518a0a3d6198249fb1490ab4a08f33ec47827e85db";
+    })
+  ];
+
+  # Fix opengl on nox nixos system. Keep an eye on https://github.com/nix-community/home-manager/issues/3968
+  nixGL.prefix = "${nixGLIntel}/bin/nixGLIntel";
+  home.packages = [ nixGLIntel ];
+
+  programs.wezterm.package = (config.lib.nixGL.wrap pkgs.wezterm);
+}
