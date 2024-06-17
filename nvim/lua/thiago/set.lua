e300c9d8707d1fce260505258035d848e20a11c5
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-17 20:34:38 -0300

    Highlight leading spaces only

--- nvim/lua/thiago/set.lua
+++ nvim/lua/thiago/set.lua
@@ -16,7 +16,7 @@ vim.opt.cursorline = true
 -- CHARS --
 vim.opt.showbreak = "↪ "
 vim.opt.list = true
-vim.opt.listchars = "tab:▎·,trail:."
+vim.opt.listchars = "tab:▎·,trail:.,lead:."
 
 -- RIGHT COLUMN --
 vim.opt.signcolumn = "yes"
