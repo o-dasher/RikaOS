c9fb282890b7eb1602ff7e357f66eaac69d5762b
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-18 11:32:38 -0400

    Better neovim notifications

---
+++ nvim/lua/thiago/lazy/notify.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		enabled = true,
+		lazy = false,
+		opts = {
+			fps = 60,
+			background_color = "#000000",
+			render = "default",
+			timeout = 1000,
+			topDown = true,
+		},
+		keys = {
+			{
+				"<leader>dn",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "[D]ismiss [N]otifications",
+			},
+		},
+	},
+}
