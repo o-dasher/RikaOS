f7488ab1e937a0393abab25573b585f8bb53ced8
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-07-12 04:37:21 -0300

    Split my utils into different files

--- nixo/flake.nix
+++ nixo/flake.nix
@@ -30,7 +30,6 @@
     }@inputs:
     let
       inherit (import ./config/myconfig.nix) username hostName;
-
       define_hm =
         base:
         home-manager.lib.homeManagerConfiguration {
@@ -41,36 +40,7 @@
           ] ++ base;
           extraSpecialArgs = {
             inherit inputs;
-            utils = {
-              css =
-                let
-                  theme_string = merge: "@theme_${merge}";
-                  gen_css_fn = fn_name: args: "${fn_name}(${args})";
-                  apply_numeric_css_fn =
-                    fn_name: property: value:
-                    gen_css_fn fn_name "${property}, ${toString value}";
-                in
-                {
-                  font_definition = "font-family: JetBrainsMono;";
-                  apply_numeric_css_fn = apply_numeric_css_fn;
-                  alpha_fn = apply_numeric_css_fn "alpha";
-                  shade_fn = apply_numeric_css_fn "shade";
-                  gen_css_fn = gen_css_fn;
-                  theme = {
-                    bg_color = theme_string "bg_color";
-                    fg_color = theme_string "fg_color";
-                    base_color = theme_string "base_color";
-                    text_color = theme_string "text_color";
-                    selected_bg_color = theme_string "selected_bg_color";
-                    selected_fg_color = theme_string "selected_fg_color";
-                  };
-                };
-              prefixset =
-                prefix: kvpairs:
-                builtins.mapAttrs (
-                  name: value: if builtins.typeOf prefix == "lambda" then prefix value else prefix + " " + value
-                ) kvpairs;
-            };
+            utils = import ./home/rika/utils;
           };
         };
     in
