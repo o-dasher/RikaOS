9b9fa29ea25531a200b80e9ba2e141b2723766bf
o-dasher <88356162+o-dasher@users.noreply.github.com.>
2025-02-09 20:40:30 -0300

    Change hostname

--- nixo/system/home/settings.nix
+++ nixo/system/home/settings.nix
@@ -3,7 +3,7 @@
   username = "thiago";
 
   # The system host name.
-  hostName = "nixos";
+  hostName = "thiagohome";
 
   # Do not modify the variable below.
   # We're using it to make sure that home-manager's stateVersion is in sync with the system's stateVersion.
