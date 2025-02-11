9e8089d85c907e4351941065b690b06a47552b24
o-dasher <88356162+o-dasher@users.noreply.github.com.>
2025-02-11 20:31:26 -0300

    Fix config

--- nixo/system/homeserver/configuration.nix
+++ nixo/system/homeserver/configuration.nix
@@ -30,7 +30,7 @@ in
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
-      age.generateKey = true;
+      generateKey = true;
     };
     secrets = {
       ipv6prefix = { };
