c6abd0e86878af814f5a9c1e7aea52189d51b066
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-02-21 03:15:07 +0000

    fix: fix timezone

--- nixo/system/modules/user_preferences.nix
+++ nixo/system/modules/user_preferences.nix
@@ -5,7 +5,7 @@
   };
 
   config = lib.mkIf (config.userPreferences.enable) {
-    time.timeZone = "Brazil/East";
+    time.timeZone = "Brazil/West";
     i18n.defaultLocale = "en_US.UTF-8";
     console.keyMap = "br-abnt2";
   };
