dbbaa5db9df262fdbae8dcd1cc166ddf3ebf875d
o-dasher <88356162+o-dasher@users.noreply.github.com.>
2025-02-22 14:18:00 -0400

    Adds a gnome module for my pc

---
+++ nixo/home/modules/gnome/default.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+{
+  options.gnome = with lib; {
+    enable = mkEnableOption "gnome";
+  };
+
+  config = lib.mkIf (config.gnome.enable) {
+    dconf.settings = with lib; {
+      "org/gnome/desktop/input-sources" = {
+        show-all-sources = true;
+        sources = [
+          (mkTuple [
+            "xkb"
+            "br"
+          ])
+        ];
+      };
+    };
+  };
+}
