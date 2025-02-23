ac994df86353059ad4c364ab72a27829f27b7a23
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-02-23 17:18:25 +0000

    refactor: simplify code to do selective symlink of directories

--- nixo/home/modules/neovim/default.nix
+++ nixo/home/modules/neovim/default.nix
@@ -6,59 +6,66 @@
 }:
 {
   options.neovim.enable = lib.mkEnableOption "neovim";
-  config = lib.mkIf config.neovim.enable {
-    xdg.configFile = {
-      "nvim/lua" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink ../../../../nvim/lua;
-      };
-
-      "nvim/ftplugin" = {
-        recursive = true;
-        source = config.lib.file.mkOutOfStoreSymlink ../../../../nvim/ftplugin;
-      };
-    };
-
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      extraLuaConfig = ''
-        require("thiago")
-      '';
-      extraPackages = with pkgs; [
-        # LSP
-        lua-language-server
-        nixd
-        phpactor
-        yaml-language-server
-        ccls
-        pyright
-        ruff-lsp
-        texlab
-        nodePackages.intelephense
-        nodePackages."@tailwindcss/language-server"
-        nodePackages.vscode-langservers-extracted
-        # typescript support
-        svelte-language-server
-        typescript
-        nodePackages.typescript-language-server
-
-        # latex
-        texliveFull
-        nodejs
-
-        # Some tools
-        tree-sitter # Tree-sitting
-        ripgrep # Telescope fzf
-        gcc # Installing tree-sitteer grammars
-        luarocks
-
-        # global formatters
-        prettierd
-        nixfmt-rfc-style
-        stylua
+  config =
+    let
+      selectiveSymLink =
+        from: to: paths:
+        lib.mkMerge (
+          map (path: {
+            "${to}/${path}" = {
+              recursive = true;
+              source = config.lib.file.mkOutOfStoreSymlink (builtins.toPath /. + "${from}/${path}");
+            };
+          }) paths
+        );
+    in
+    lib.mkIf config.neovim.enable {
+      home.file = selectiveSymLink ../../../../nvim ".config/nvim" [
+        "lua"
+        "ftplugin"
+        "lazylock.json"
       ];
+
+      programs.neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+        extraLuaConfig = ''
+          require("thiago")
+        '';
+        extraPackages = with pkgs; [
+          # LSP
+          lua-language-server
+          nixd
+          phpactor
+          yaml-language-server
+          ccls
+          pyright
+          ruff-lsp
+          texlab
+          nodePackages.intelephense
+          nodePackages."@tailwindcss/language-server"
+          nodePackages.vscode-langservers-extracted
+          # typescript support
+          svelte-language-server
+          typescript
+          nodePackages.typescript-language-server
+
+          # latex
+          texliveFull
+          nodejs
+
+          # Some tools
+          tree-sitter # Tree-sitting
+          ripgrep # Telescope fzf
+          gcc # Installing tree-sitteer grammars
+          luarocks
+
+          # global formatters
+          prettierd
+          nixfmt-rfc-style
+          stylua
+        ];
+      };
     };
-  };
 }
