0df3c126473398baf8615b52be0af2ab935afbf0
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-02-24 03:04:55 +0000

    feat: do not have portals on server profile

--- nixo/system/gensokyo/profiles/thiago/default.nix
+++ nixo/system/gensokyo/profiles/thiago/default.nix
@@ -5,11 +5,6 @@
   ];
 
   xdgSetup.enable = true;
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gnome
-    xdg-desktop-portal-gtk
-  ];
-
   home.packages = with pkgs; [
     openjdk
     tmux
