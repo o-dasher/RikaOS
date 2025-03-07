0130b91ad5dc956f84dfc17d2ab4dc10049b640e
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-07 01:25:28 +0000

    feat(gaming): add a small configuration for multiuser steam symlinks setup

--- nixo/home/modules/games/default.nix
+++ nixo/home/modules/games/default.nix
@@ -9,25 +9,40 @@
     enable = mkEnableOption "games";
     osu.enable = mkEnableOption "osu-lazer";
     minecraft.enable = mkEnableOption "minecraft";
+    steam.enable = mkEnableOption "steam";
   };
 
-  config = lib.mkIf config.games.enable {
-    home.packages = with pkgs; [
-      mangohud
-      goverlay
-      (heroic.override {
-        extraPkgs = pkgs: [
-          pkgs.gamescope
+  config = lib.mkIf config.games.enable (
+    lib.mkMerge [
+      {
+        home.packages = with pkgs; [
+          mangohud
+          goverlay
+          (heroic.override {
+            extraPkgs = pkgs: [
+              pkgs.gamescope
+            ];
+          })
+          (lib.mkIf config.games.osu.enable osu-lazer-bin)
+          (lib.mkIf config.games.minecraft.enable (
+            prismlauncher.override {
+              jdks = [
+                temurin-bin-21
+              ];
+            }
+          ))
         ];
+      }
+      (lib.mkIf config.games.steam.enable {
+        home.file."Games/Steam/steamapps/common".source =
+          config.lib.file.mkOutOfStoreSymlink "/steam-games/SteamLibrary/steamapps/common";
+
+        home.file."Games/Steam/steamapps/downloading".source =
+          config.lib.file.mkOutOfStoreSymlink "/steam-games/SteamLibrary/steamapps/downloading";
+
+        home.file."Games/Steam/steamapps/shadercache".source =
+          config.lib.file.mkOutOfStoreSymlink "/steam-games/SteamLibrary/steamapps/shadercache";
       })
-      (lib.mkIf config.games.osu.enable osu-lazer-bin)
-      (lib.mkIf config.games.minecraft.enable (
-        prismlauncher.override {
-          jdks = [
-            temurin-bin-21
-          ];
-        }
-      ))
-    ];
-  };
+    ]
+  );
 }
