43e0e4eb16aacaddc478ab6757aab760cb9eb7f1
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-06 21:40:31 -0400

    Applying a bit of DRY to the symlinks setup

--- nixo/home/modules/applications/neovim/default.nix
+++ nixo/home/modules/applications/neovim/default.nix
@@ -2,26 +2,19 @@
   pkgs,
   config,
   lib,
+  utils,
   ...
 }:
+let
+in
 {
   options.neovim.enable = lib.mkEnableOption "neovim";
-  config =
-    let
-      selectiveSymLink =
-        from: to: paths: opts:
-        lib.mkMerge (
-          map (path: {
-            "${to}/${path}" = {
-              source = config.lib.file.mkOutOfStoreSymlink (builtins.toPath /. + "${from}/${path}");
-            } // opts;
-          }) paths
-        );
-
-      symLinkNeovim = selectiveSymLink ../../../../../nvim ".config/nvim";
-    in
-    lib.mkIf config.neovim.enable {
-      home.file = lib.mkMerge [
+  config = lib.mkIf config.neovim.enable {
+    home.file =
+      let
+        symLinkNeovim = utils.selectiveSymLink ../../../../../nvim ".config/nvim";
+      in
+      lib.mkMerge [
         (symLinkNeovim [
           "lua"
           "ftplugin"
@@ -29,46 +22,46 @@
         (symLinkNeovim [ "lazy-lock.json" ] { })
       ];
 
-      programs.neovim = {
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-        extraLuaConfig = ''
-          require("thiago")
-        '';
-        extraPackages = with pkgs; [
-          # LSP
-          lua-language-server
-          nixd
-          phpactor
-          yaml-language-server
-          ccls
-          pyright
-          ruff-lsp
-          texlab
-          nodePackages.intelephense
-          nodePackages."@tailwindcss/language-server"
-          nodePackages.vscode-langservers-extracted
-          # typescript support
-          svelte-language-server
-          typescript
-          nodePackages.typescript-language-server
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      extraLuaConfig = ''
+        require("thiago")
+      '';
+      extraPackages = with pkgs; [
+        # LSP
+        lua-language-server
+        nixd
+        phpactor
+        yaml-language-server
+        ccls
+        pyright
+        ruff-lsp
+        texlab
+        nodePackages.intelephense
+        nodePackages."@tailwindcss/language-server"
+        nodePackages.vscode-langservers-extracted
+        # typescript support
+        svelte-language-server
+        typescript
+        nodePackages.typescript-language-server
 
-          # latex
-          texliveFull
-          nodejs
+        # latex
+        texliveFull
+        nodejs
 
-          # Some tools
-          tree-sitter # Tree-sitting
-          ripgrep # Telescope fzf
-          gcc # Installing tree-sitteer grammars
-          luarocks
+        # Some tools
+        tree-sitter # Tree-sitting
+        ripgrep # Telescope fzf
+        gcc # Installing tree-sitteer grammars
+        luarocks
 
-          # global formatters
-          prettierd
-          nixfmt-rfc-style
-          stylua
-        ];
-      };
+        # global formatters
+        prettierd
+        nixfmt-rfc-style
+        stylua
+      ];
     };
+  };
 }
