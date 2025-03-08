7c5c9dd77d0393c09588322dd2a08f509c8ae8c1
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-08 09:18:13 +0000

    feat(desktop/hyprland): enable direct scanout support

--- nixo/home/modules/desktop/hyprland/default.nix
+++ nixo/home/modules/desktop/hyprland/default.nix
@@ -56,8 +56,9 @@ in
           (lib.getExe pkgs.lxqt.lxqt-policykit)
           "[workspace 9 silent] ${lib.getExe pkgs.qbittorrent}"
         ];
+        debug.disable_logs = false;
         monitor = [ "HDMI-A-1,1920x1080@239.76,0x0,1" ];
-        # render.direct_scanout = true; maybe some day, I still get graphical glitches
+        render.direct_scanout = true;
         windowrulev2 = [ "immediate, fullscreen:1" ];
         misc = {
           render_ahead_of_time = true;
