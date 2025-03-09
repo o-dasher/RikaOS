77138b1d63bb4f461d7dff1b3fda944bfa559c04
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-09 07:57:16 -0400

    Better ghostty styling and remove fancy syntax

--- nixo/home/modules/terminal/ghostty.nix
+++ nixo/home/modules/terminal/ghostty.nix
@@ -2,14 +2,14 @@
   lib,
   pkgs,
   config,
-  ghostty,
+  inputs,
   ...
 }:
 {
   config = lib.mkIf (config.terminal.enable && config.terminal.ghostty.enable) {
     programs.ghostty = {
       enable = true;
-      package = ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
       settings = lib.mkMerge [
         {
           font-size = 14;
@@ -17,10 +17,6 @@
           clipboard-read = "allow";
           clipboard-paste-protection = false;
         }
-        (lib.mkIf (!config.gnome.enable) {
-          gtk-titlebar = false;
-          window-decoration = false;
-        })
       ];
     };
   };
