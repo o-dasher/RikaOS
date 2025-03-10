71d93eab4f32defb6dcee55672615ad0cd6e1d58
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-09 23:41:58 -0400

    Fix icons on fuzzel

--- nixo/home/modules/desktop/hyprland/fuzzel.nix
+++ nixo/home/modules/desktop/hyprland/fuzzel.nix
@@ -1,18 +1,26 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 {
   options.desktop.hyprland.fuzzel.enable = (lib.mkEnableOption "fuzzel") // {
     default = true;
   };
 
-  config.programs.fuzzel =
-    lib.mkIf (config.desktop.hyprland.enable && config.desktop.hyprland.fuzzel.enable)
-      {
-        enable = true;
-        settings = {
-          main = {
-            font = lib.mkForce "JetbrainsMono:size=16";
-            width = 48;
-          };
+  config = lib.mkIf (config.desktop.hyprland.enable && config.desktop.hyprland.fuzzel.enable) {
+    home.packages = [
+      config.stylix.iconTheme.package
+    ];
+    programs.fuzzel = {
+      enable = true;
+      settings = {
+        main = {
+          font = lib.mkForce "JetbrainsMono:size=16";
+          width = 48;
+          icon-theme = config.stylix.iconTheme.${config.stylix.polarity};
         };
       };
+    };
+  };
 }
