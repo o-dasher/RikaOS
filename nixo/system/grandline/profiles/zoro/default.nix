e29e189cf5c48656646f89e770a674ff93fe8e86
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-14 21:45:18 +0000

    Whatever man, change dressing to snacks and do some stuff on my wsl conf

--- nixo/system/grandline/profiles/zoro/default.nix
+++ nixo/system/grandline/profiles/zoro/default.nix
@@ -11,5 +11,19 @@
   neovim.enable = true;
   terminal.enable = true;
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    hyfetch = {
+      enable = true;
+      settings = {
+        preset = "bisexual";
+        mode = "rgb";
+        color_align.mode = "horizontal";
+      };
+    };
+  };
 }
