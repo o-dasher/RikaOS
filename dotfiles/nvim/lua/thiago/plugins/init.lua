c957ab5b442c8469745f188a3a9e767c6546f86a
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-03-16 19:11:51 -0400

    Improve my overall neovim config and fix notifications

--- dotfiles/nvim/lua/thiago/plugins/init.lua
+++ dotfiles/nvim/lua/thiago/plugins/init.lua
@@ -2,7 +2,6 @@ require("lze").load({
 	{ import = "thiago.plugins.blink_cmp" },
 	{ import = "thiago.plugins.lsp" },
 	{ import = "thiago.plugins.conform" },
-	{ import = "thiago.plugins.fidget" },
 	{ import = "thiago.plugins.harpoon" },
 	{ import = "thiago.plugins.lazygit" },
 	{ import = "thiago.plugins.nvim_lint" },
@@ -13,10 +12,15 @@ require("lze").load({
 	{ import = "thiago.plugins.telescope" },
 	{ import = "thiago.plugins.treesitter" },
 	{ import = "thiago.plugins.vimtex" },
+	{ import = "thiago.plugins.mini" },
 	{
-		"nvim-lua/plenary.nvim",
+		"plenary.nvim",
 		dep_of = { "harpoon", "lazygit", "telescope" },
 	},
-	{ "nvim-tree/nvim-web-devicons", dep_of = { "oil" } },
-	{ "nvim-telescope/telescope-live-grep-args.nvim", dep_of = "telescope" },
+	{ "nvim-web-devicons", dep_of = { "oil" } },
+	{ "telescope-live-grep-args.nvim", dep_of = "telescope" },
+	{
+		"rose-pine",
+		colorscheme = "rose-pine",
+	},
 })
