148779ba877725ced2624cb6f30dd16e7bcde3eb
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-08-22 17:18:59 -0400

    Adds tavily api key environment variable for avante nvim

--- nixo/secrets/secrets.nix
+++ nixo/secrets/secrets.nix
@@ -1,10 +1,17 @@
 let
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlJb/U8u+FDcHncm5l6rtg11mUrsaQDYsgLgqrXKTSO root@hinamizawa";
-  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPAM12J0/Z/otlj0f6p6wvrEGFMGiBtcVb9zD7HjRVp rika@hinamizawa";
+  rika_system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlJb/U8u+FDcHncm5l6rtg11mUrsaQDYsgLgqrXKTSO root@hinamizawa";
+  rika_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPAM12J0/Z/otlj0f6p6wvrEGFMGiBtcVb9zD7HjRVp rika@hinamizawa";
+
+  gpm_user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFrRfALDfIg8JumMXk4j4LQyEIKwPf5pR225wj1hst0";
 in
 {
   "playit-secret.age".publicKeys = [
-    user
-    system
+    rika_user
+    rika_system
+  ];
+
+  "tavily-api-key.age".publicKeys = [
+    rika_user
+    gpm_user
   ];
 }
