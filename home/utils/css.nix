e85f0de795967bd595be66fa0da33b3abee44a1d
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-09-28 22:12:13 +0000

    refactor(desktop/hyprland): move the tailwindcss helper function more globally

--- home/utils/css.nix
+++ home/utils/css.nix
@@ -9,4 +9,57 @@ in
   apply_numeric_css_fn = apply_numeric_css_fn;
   alpha_fn = apply_numeric_css_fn "alpha";
   shade_fn = apply_numeric_css_fn "shade";
+
+  tailwindCSS =
+    pkgs: colors: content:
+    pkgs.runCommand "waybar-style.css"
+      {
+        nativeBuildInputs = [ pkgs.nodePackages.tailwindcss ];
+        tailwindConfig =
+          let
+            inherit (colors)
+              base00
+              base01
+              base02
+              base03
+              base04
+              base05
+              base06
+              base07
+              base08
+              base09
+              base0A
+              base0B
+              base0C
+              base0D
+              base0E
+              base0F
+              ;
+          in
+          pkgs.writeText "tailwind.config.js"
+            # js
+            ''
+              module.exports = {
+                content: ["./input.css"],
+                theme: {
+                  extend: {
+                    colors: {
+                      base00: "#${base00}", base01: "#${base01}", base02: "#${base02}", base03: "#${base03}",
+                      base04: "#${base04}", base05: "#${base05}", base06: "#${base06}", base07: "#${base07}",
+                      base08: "#${base08}", base09: "#${base09}", base0A: "#${base0A}", base0B: "#${base0B}",
+                      base0C: "#${base0C}", base0D: "#${base0D}", base0E: "#${base0E}", base0F: "#${base0F}"
+                    }
+                  }
+                },
+                plugins: [],
+              }
+            '';
+      }
+      ''
+        ln -s $tailwindConfig tailwind.config.js
+        cat > input.css <<EOF
+        ${content}
+        EOF
+        ${pkgs.nodePackages.tailwindcss}/bin/tailwindcss -i input.css -o $out
+      '';
 }
