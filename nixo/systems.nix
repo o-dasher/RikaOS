c424ece78a8f66a49ea4e7609c044fcd4ad5682d
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-09-28 02:54:21 -0400

    Simplify the flake further

---
+++ nixo/systems.nix
@@ -0,0 +1,37 @@
+{
+  # The attribute name (e.g., "hinamizawa") is used as the `targetHostName`.
+  # The `hostName` attribute is used for the `nixosConfigurations` output name,
+  # and it defaults to the `targetHostName` if not specified.
+  systemCfgs = {
+    hinamizawa = {
+      state = "24.11";
+      profiles = {
+        rika = "rika";
+        satoko = "satoko";
+      };
+    };
+    gensokyo = {
+      state = "24.05";
+      profiles = {
+        nue = "thiago";
+      };
+    };
+    grandline = {
+      state = "24.05";
+      profiles = {
+        zoro = "zoro";
+      };
+    };
+  };
+
+  # Home manager only configurations
+  homeCfgs = {
+    silly = {
+      hostName = "gpmecatronica-System-Product-Name";
+      state = "24.05";
+      profiles = {
+        gleep = "thiagogpm";
+      };
+    };
+  };
+}
