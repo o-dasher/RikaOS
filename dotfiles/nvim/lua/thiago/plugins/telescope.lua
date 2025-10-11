dd9a260797988ef42134766678e8e1a9127d7897
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-10-10 22:53:48 -0400

    Proper lazy loading telescope

--- dotfiles/nvim/lua/thiago/plugins/telescope.lua
+++ dotfiles/nvim/lua/thiago/plugins/telescope.lua
@@ -3,10 +3,17 @@ local function t()
 end
 
 return {
-	"nvim-telescope/telescope.nvim",
+	"telescope.nvim",
+	on_require = "telescope",
+	load = function(name)
+		vim.cmd.packadd(name)
+		vim.cmd.packadd("telescope-live-grep-args.nvim")
+	end,
 	after = function(_, _)
+		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		require("telescope").setup({
+
+		telescope.setup({
 			defaults = {
 				mappings = {
 					i = {
@@ -16,6 +23,8 @@ return {
 				},
 			},
 		})
+
+		telescope.load_extension("live_grep_args")
 	end,
 	keys = {
 		{
