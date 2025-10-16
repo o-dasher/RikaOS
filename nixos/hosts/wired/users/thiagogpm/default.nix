229abd3ca3907174cf1a20c0bd4bc2b6b15d1325
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-10-16 04:05:50 +0000

    feat(hosts/hinamizawa): make nixgl setup a bit more generic

--- nixos/hosts/wired/users/thiagogpm/default.nix
+++ nixos/hosts/wired/users/thiagogpm/default.nix
@@ -1,9 +1,6 @@
 {
-  config,
-  lib,
   pkgs,
   inputs,
-  nixgl,
   ...
 }:
 {
@@ -13,17 +10,8 @@
 
   targets.genericLinux.enable = true;
 
-  xdgSetup.enable = true;
-  nixSetup.enable = true;
-
-  nixGL.packages = nixgl.packages;
-  sharedFolders.configurationRoot = "~/Programming/RikaOS";
-
   development.enable = true;
   desktop.hyprland.enable = true;
-
-  programs.ghostty.package = config.lib.nixGL.wrap pkgs.ghostty;
-  wayland.windowManager.hyprland.package = lib.mkForce (config.lib.nixGL.wrap pkgs.hyprland);
 
   home.packages = with pkgs; [
     inputs.zen-browser.packages.${system}.twilight
