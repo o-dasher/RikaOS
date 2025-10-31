d8a055355d7349a4d69e12a0386ca251b349038c
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-10-31 20:18:33 +0000

    feat: github

--- modules/home/features/dev/git.nix
+++ modules/home/features/dev/git.nix
@@ -1,8 +1,15 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   options.dev.git.enable = lib.mkEnableOption "git";
 
   config = lib.mkIf config.dev.git.enable {
-    programs.gh.enable = true;
+    home.packages = [
+      pkgs.gh
+    ];
   };
 }
