44505818ce111c3340176e2de43279180a74405e
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-24 19:34:36 -0400

    Remove unused

--- modules/home/features/default.nix
+++ modules/home/features/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
-    ./applications
     ./cli
     ./desktop
     ./editors
--- modules/home/profiles/security.nix
+++ modules/home/profiles/security.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   options.profiles.security = {
     enable = lib.mkEnableOption "security profile";
