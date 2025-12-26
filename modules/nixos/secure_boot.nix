025af6a2a09d794c1f5df68c4b43ab95045a8641
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-26 07:14:23 -0400

    Better secure boot setup

--- modules/nixos/secure_boot.nix
+++ modules/nixos/secure_boot.nix
@@ -21,19 +21,27 @@
 
     # Use the systemd-boot EFI boot loader.
     boot = {
-      # Lanzaboote currently replaces the systemd-boot module.
-      # This setting is usually set to true in configuration.nix
-      # generated at installation time. So we force it to false
-      # for now.
-      loader.systemd-boot = {
-        enable = lib.mkForce false;
-        consoleMode = "keep";
-        configurationLimit = 3;
-        edk2-uefi-shell.enable = true;
+      loader = {
+        efi.canTouchEfiVariables = true;
+        # Lanzaboote currently replaces the systemd-boot module.
+        # This setting is usually set to true in configuration.nix
+        # generated at installation time. So we force it to false
+        # for now.
+        systemd-boot = {
+          enable = lib.mkForce false;
+          consoleMode = "max";
+          configurationLimit = 3;
+          edk2-uefi-shell.enable = true;
+        };
       };
       lanzaboote = {
         enable = true;
         pkiBundle = "/var/lib/sbctl";
+        autoGenerateKeys.enable = true;
+        autoEnrollKeys = {
+          enable = true;
+          autoReboot = true;
+        };
       };
     };
   };
