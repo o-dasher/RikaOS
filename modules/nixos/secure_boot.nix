ce18f52047f689efa74a58145d61ed5bdf8671d0
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-28 09:19:15 -0400

    Adds automatic disk encryption unlocking capabilities

--- modules/nixos/secure_boot.nix
+++ modules/nixos/secure_boot.nix
@@ -8,18 +8,33 @@
 
   options.secureBoot = with lib; {
     enable = mkEnableOption "secureBoot";
+    # Reference for auto unlocking encrypted drive: https://discourse.nixos.org/t/full-disk-encryption-tpm2/29454
+    encryptionUnlock.enable = mkEnableOption "Unlock encrypted drives automatically";
   };
 
-  config = lib.mkIf config.secureBoot.enable {
-    environment.systemPackages = [ pkgs.sbctl ];
+  config =
+    let
+      cfg = config.secureBoot;
+    in
+    lib.mkIf cfg.enable {
+      environment.systemPackages = [
+        pkgs.sbctl
+      ];
 
-    boot.loader = {
-      efi.canTouchEfiVariables = true;
-      limine = {
-        enable = true;
-        secureBoot.enable = true;
-        maxGenerations = 3;
+      boot = {
+        initrd.systemd = lib.mkIf (cfg.encryptionUnlock.enable) {
+          enable = true;
+          tpm2.enable = true;
+        };
+
+        loader = {
+          efi.canTouchEfiVariables = true;
+          limine = {
+            enable = true;
+            secureBoot.enable = true;
+            maxGenerations = 3;
+          };
+        };
       };
     };
-  };
 }
