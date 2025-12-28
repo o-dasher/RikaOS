e273e3801831336b1ae5f82d38a20d34fdfe2a12
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-28 17:13:33 +0000

    feat: better fonts

--- modules/home/features/desktop/fonts.nix
+++ modules/home/features/desktop/fonts.nix
@@ -10,10 +10,18 @@
   };
 
   config = lib.mkIf config.desktop.fonts.enable {
+    fonts.fontconfig.enable = true;
     home.packages = with pkgs; [
-      jetbrains-mono
+      # Coding / Terminal
       nerd-fonts.fira-mono
       nerd-fonts.jetbrains-mono
+
+      # Essential for Web & Documents
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      dejavu_fonts
     ];
   };
 }
