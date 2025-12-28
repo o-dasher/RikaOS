ed98aba496603c5833562f9668674f4f50eb217b
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-28 03:41:16 -0400

    Fixing slow exit times on gracefully exit session

--- modules/nixos/features/desktop/hyprland.nix
+++ modules/nixos/features/desktop/hyprland.nix
@@ -11,6 +11,18 @@
   };
 
   config = lib.mkIf config.features.desktop.hyprland.enable {
+    # We don't want to wait for way too long.
+    systemd.user.extraConfig = # ini
+      ''
+        DefaultTimeoutStopSec=3s
+        DefaultTimeoutStartSec=3s
+      '';
+
+    systemd.settings.Manager = {
+      DefaultTimeoutStopSec = "3s";
+      DefaultTimeoutStartSec = "3s";
+    };
+
     programs = {
       # BUG: workaround https://github.com/hyprwm/Hyprland/discussions/12661
       uwsm.waylandCompositors.hyprland.binPath = lib.mkForce "/run/current-system/sw/bin/start-hyprland";
