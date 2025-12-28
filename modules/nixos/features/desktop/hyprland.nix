f13d65752b5f3d55ea42b0882a7500a122b7359b
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-27 23:05:04 -0400

    Simply UWSM workaround

--- modules/nixos/features/desktop/hyprland.nix
+++ modules/nixos/features/desktop/hyprland.nix
@@ -12,19 +12,12 @@
 
   config = lib.mkIf config.features.desktop.hyprland.enable {
     programs = {
-      uwsm = {
-        enable = true;
-        waylandCompositors = {
-          hyprland = {
-            prettyName = "Hyprland";
-            comment = "Hyprland compositor managed by UWSM";
-            binPath = "/run/current-system/sw/bin/start-hyprland";
-          };
-        };
-      };
+      # BUG: workanround https://github.com/hyprwm/Hyprland/discussions/12661
+      uwsm.waylandCompositors.hyprland.binPath = lib.mkForce "/run/current-system/sw/bin/start-hyprland";
 
       hyprland = {
         enable = true;
+        withUWSM = true;
         package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         portalPackage =
           inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
