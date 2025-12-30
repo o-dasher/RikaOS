b0356c68475e681a4cf27333c4e27457d260fae4
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-30 08:38:44 -0400

    Split hm logic from theming more properly

--- modules/home/features/desktop/theme/default.nix
+++ modules/home/features/desktop/theme/default.nix
@@ -1,8 +1,29 @@
-# Theme module for home-manager
-# Imports from common theme module and includes utils
+{ pkgs, ... }:
+let
+  cursorName = "BreezeX-RosePine-Linux";
+  cursorPackage = pkgs.rose-pine-cursor;
+in
 {
   imports = [
     ../../../../common/theme
   ];
-}
 
+  home.pointerCursor = {
+    name = cursorName;
+    package = cursorPackage;
+  };
+
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = cursorName;
+      package = cursorPackage;
+    };
+  };
+
+  stylix = {
+    icons.enable = true;
+    targets.nixcord.enable = false;
+    targets.zen-browser.profileNames = [ "default" ];
+  };
+}
