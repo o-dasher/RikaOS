0a8b0450ac704b588bdd4a55c49734b830151c32
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-30 08:41:29 -0400

    Simplify cursor theming logic

--- modules/home/features/desktop/theme/default.nix
+++ modules/home/features/desktop/theme/default.nix
@@ -1,29 +1,21 @@
 { pkgs, ... }:
 let
-  cursorName = "BreezeX-RosePine-Linux";
-  cursorPackage = pkgs.rose-pine-cursor;
 in
 {
   imports = [
     ../../../../common/theme
   ];
 
-  home.pointerCursor = {
-    name = cursorName;
-    package = cursorPackage;
-  };
-
-  gtk = {
-    enable = true;
-    cursorTheme = {
-      name = cursorName;
-      package = cursorPackage;
-    };
-  };
-
   stylix = {
     icons.enable = true;
-    targets.nixcord.enable = false;
-    targets.zen-browser.profileNames = [ "default" ];
+    targets = {
+      nixcord.enable = false;
+      zen-browser.profileNames = [ "default" ];
+    };
+    cursor = {
+      name = "BreezeX-RosePine-Linux";
+      package = pkgs.rose-pine-cursor;
+      size = 16;
+    };
   };
 }
