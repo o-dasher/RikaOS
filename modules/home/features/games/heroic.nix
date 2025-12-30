06527ca86a83b07a9f5fbd966cafea8b2a9d6072
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-30 03:44:05 +0000

    feat(gaming): add gamemode for heroic

--- modules/home/features/games/heroic.nix
+++ modules/home/features/games/heroic.nix
@@ -1,13 +1,20 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   options.games.heroic.enable = lib.mkEnableOption "heroic";
 
   config = lib.mkIf (config.games.enable && config.games.heroic.enable) {
     home.packages = [
       (pkgs.heroic.override {
-        extraPkgs = pkgs: [
-          pkgs.gamescope
-        ];
+        extraPkgs =
+          pkgs: with pkgs; [
+            gamescope
+            gamemode
+          ];
       })
     ];
   };
