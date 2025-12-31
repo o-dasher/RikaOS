93dfd667d36f86ec1f05a3ce531c879e47b27bd6
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 08:18:53 +0000

    refactor(flake): simplify profiles declarations

--- flake.nix
+++ flake.nix
@@ -141,16 +141,16 @@
       systemCfgs = {
         hinamizawa = {
           state = "24.11";
-          profiles = {
-            rika = "rika";
-            satoko = "satoko";
-          };
+          users = [
+            "rika"
+            "satoko"
+          ];
         };
         gensokyo = {
           state = "24.05";
-          profiles = {
-            nue = "thiago";
-          };
+          users = [
+            "thiago"
+          ];
         };
       };
 
@@ -158,7 +158,7 @@
         # wired = {
         #   hostName = "gpmecatronica-System-Product-Name";
         #   state = "24.05";
-        #   profiles = {
+        #   users = {
         #     thiagogpm = "thiagogpm";
         #   };
         # };
@@ -202,20 +202,22 @@
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 extraSpecialArgs = commonArgs;
-                users = nixpkgs.lib.mapAttrs' (
-                  _: username:
-                  nixpkgs.lib.nameValuePair username (
-                    { ... }:
-                    {
-                      imports = commonHomeModules ++ [ ./hosts/${hostName}/users/${username} ];
-                      home = {
-                        inherit username;
-                        homeDirectory = "/home/${username}";
-                        stateVersion = systemCfg.state;
-                      };
-                    }
-                  )
-                ) systemCfg.profiles;
+                users = nixpkgs.lib.listToAttrs (
+                  map (
+                    username:
+                    nixpkgs.lib.nameValuePair username (
+                      { ... }:
+                      {
+                        imports = commonHomeModules ++ [ ./hosts/${hostName}/users/${username} ];
+                        home = {
+                          inherit username;
+                          homeDirectory = "/home/${username}";
+                          stateVersion = systemCfg.state;
+                        };
+                      }
+                    )
+                  ) systemCfg.users
+                );
               };
             }
           ];
@@ -263,10 +265,10 @@
             users = nixpkgs.lib.flatten (
               nixpkgs.lib.mapAttrsToList (
                 hostName: cfg:
-                nixpkgs.lib.mapAttrsToList (_: username: {
+                map (username: {
                   name = username;
                   value = mkHome hostName cfg username;
-                }) cfg.profiles
+                }) cfg.users
               ) homeCfgs
             );
           in
