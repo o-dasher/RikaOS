808b05850c1016617d3d0be5ee3d738752f9bdaf
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 18:26:05 +0000

    feat(desktop/waybar): using more tailwind

--- modules/lib/utils.nix
+++ modules/lib/utils.nix
@@ -1,11 +1,4 @@
 { lib, config, ... }:
-let
-  gen_css_fn = fn_name: args: "${fn_name}(${args})";
-  apply_numeric_css_fn =
-    fn_name: property: value:
-    gen_css_fn fn_name "${property}, ${toString value}";
-
-in
 {
   options.rika.utils = lib.mkOption {
     type = lib.types.attrs;
@@ -35,63 +28,57 @@ in
         name: value: if builtins.typeOf prefix == "lambda" then prefix value else prefix + " " + value
       ) kvpairs;
 
-    css = {
-      inherit apply_numeric_css_fn;
-      alpha_fn = apply_numeric_css_fn "alpha";
-      shade_fn = apply_numeric_css_fn "shade";
-
-      tailwindCSS =
-        pkgs: colors: content:
-        pkgs.runCommand "tailwindify.css"
-          {
-            nativeBuildInputs = [ pkgs.nodePackages.tailwindcss ];
-            tailwindConfig =
-              let
-                inherit (colors)
-                  base00
-                  base01
-                  base02
-                  base03
-                  base04
-                  base05
-                  base06
-                  base07
-                  base08
-                  base09
-                  base0A
-                  base0B
-                  base0C
-                  base0D
-                  base0E
-                  base0F
-                  ;
-              in
-              pkgs.writeText "tailwind.config.js"
-                # js
-                ''
-                  module.exports = {
-                    content: ["./input.css"],
-                    theme: {
-                      extend: {
-                        colors: {
-                          base00: "#${base00}", base01: "#${base01}", base02: "#${base02}", base03: "#${base03}",
-                          base04: "#${base04}", base05: "#${base05}", base06: "#${base06}", base07: "#${base07}",
-                          base08: "#${base08}", base09: "#${base09}", base0A: "#${base0A}", base0B: "#${base0B}",
-                          base0C: "#${base0C}", base0D: "#${base0D}", base0E: "#${base0E}", base0F: "#${base0F}"
-                        }
+    css.tailwindCSS =
+      pkgs: colors: content:
+      pkgs.runCommand "tailwindify.css"
+        {
+          nativeBuildInputs = [ pkgs.nodePackages.tailwindcss ];
+          tailwindConfig =
+            let
+              inherit (colors)
+                base00
+                base01
+                base02
+                base03
+                base04
+                base05
+                base06
+                base07
+                base08
+                base09
+                base0A
+                base0B
+                base0C
+                base0D
+                base0E
+                base0F
+                ;
+            in
+            pkgs.writeText "tailwind.config.js"
+              # js
+              ''
+                module.exports = {
+                  content: ["./input.css"],
+                  theme: {
+                    extend: {
+                      colors: {
+                        base00: "#${base00}", base01: "#${base01}", base02: "#${base02}", base03: "#${base03}",
+                        base04: "#${base04}", base05: "#${base05}", base06: "#${base06}", base07: "#${base07}",
+                        base08: "#${base08}", base09: "#${base09}", base0A: "#${base0A}", base0B: "#${base0B}",
+                        base0C: "#${base0C}", base0D: "#${base0D}", base0E: "#${base0E}", base0F: "#${base0F}"
                       }
-                    },
-                    plugins: [],
-                  }
-                '';
-          }
-          ''
-            ln -s $tailwindConfig tailwind.config.js
-            cat > input.css <<EOF
-            ${content}
-            EOF
-            ${pkgs.nodePackages.tailwindcss}/bin/tailwindcss -i input.css -o $out
-          '';
-    };
+                    }
+                  },
+                  plugins: [],
+                }
+              '';
+        }
+        ''
+          ln -s $tailwindConfig tailwind.config.js
+          cat > input.css <<EOF
+          ${content}
+          EOF
+          ${pkgs.nodePackages.tailwindcss}/bin/tailwindcss -i input.css -o $out
+        '';
   };
 }
