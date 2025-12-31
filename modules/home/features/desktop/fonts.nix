7645327cbe5fdc6fcdd5caea44aa6cec01846a59
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 18:56:57 +0000

    fix: fix compile error

--- modules/home/features/desktop/fonts.nix
+++ modules/home/features/desktop/fonts.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   pkgs,
+  options,
   ...
 }:
 {
@@ -9,37 +10,41 @@
     default = true;
   };
 
-  config = lib.mkIf config.features.desktop.fonts.enable {
-    fonts.fontconfig.enable = true;
+  config = lib.mkIf config.features.desktop.fonts.enable (
+    lib.mkMerge [
+      (lib.optionalAttrs (options ? stylix) {
+        stylix.fonts = {
+          monospace = {
+            package = pkgs.nerd-fonts.jetbrains-mono;
+            name = "JetBrainsMono Nerd Font";
+          };
+          sansSerif = {
+            package = pkgs.noto-fonts;
+            name = "Noto Sans";
+          };
+          serif = {
+            package = pkgs.noto-fonts;
+            name = "Noto Serif";
+          };
+          sizes = {
+            desktop = 9;
+            applications = 12;
+            popups = 12;
+          };
+        };
+      })
+      {
+        fonts.fontconfig.enable = true;
+        home.packages = with pkgs; [
+          # Coding / Terminal
+          nerd-fonts.fira-mono
 
-    stylix.fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font";
-      };
-      sansSerif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Sans";
-      };
-      serif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Serif";
-      };
-      sizes = {
-        desktop = 9;
-        applications = 12;
-        popups = 12;
-      };
-    };
-
-    home.packages = with pkgs; [
-      # Coding / Terminal
-      nerd-fonts.fira-mono
-
-      # Essential for Web & Documents
-      noto-fonts-cjk-sans
-      liberation_ttf
-      dejavu_fonts
-    ];
-  };
+          # Essential for Web & Documents
+          noto-fonts-cjk-sans
+          liberation_ttf
+          dejavu_fonts
+        ];
+      }
+    ]
+  );
 }
