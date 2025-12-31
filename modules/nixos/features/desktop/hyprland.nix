c12deaaf8330c5699ff6f6bd93c77fa48b3fa3be
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 19:34:28 +0000

    feat(desktop/hyprland): less workarounds

--- modules/nixos/features/desktop/hyprland.nix
+++ modules/nixos/features/desktop/hyprland.nix
@@ -12,16 +12,11 @@
 
   config = lib.mkIf config.features.desktop.hyprland.enable {
     # We don't want to wait for way too long.
+    systemd.settings.Manager.DefaultTimeoutStopSec = "3s";
     systemd.user.extraConfig = # ini
       ''
         DefaultTimeoutStopSec=3s
-        DefaultTimeoutStartSec=3s
       '';
-
-    systemd.settings.Manager = {
-      DefaultTimeoutStopSec = "3s";
-      DefaultTimeoutStartSec = "3s";
-    };
 
     programs = {
       # BUG: workaround https://github.com/hyprwm/Hyprland/discussions/12661
