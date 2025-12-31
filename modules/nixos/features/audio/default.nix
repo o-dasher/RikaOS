4ea13c607f6cece5232b19ed11863624bfa793e0
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 13:00:58 +0000

    feat(audio): do not list every bluetooth codec

--- modules/nixos/features/audio/default.nix
+++ modules/nixos/features/audio/default.nix
@@ -17,7 +17,6 @@
       pulse.enable = true;
       jack.enable = true;
       lowLatency.enable = true;
-
       wireplumber = {
         enable = true;
         extraConfig = {
@@ -26,14 +25,6 @@
               "bluez5.enable-sbc-xq" = true;
               "bluez5.enable-msbc" = true;
               "bluez5.enable-hw-volume" = true;
-              "bluez5.codecs" = [
-                "sbc"
-                "sbc_xq"
-                "aac"
-                "ldac"
-                "aptx"
-                "aptx_hd"
-              ];
             };
           };
         };
