9eff04d66a785e37f6cfdf32469a7d5bf38463ae
o-dasher <88356162+o-dasher@users.noreply.github.com>
2025-12-31 03:20:17 -0400

    Fix eval error

--- modules/home/features/desktop/theme/default.nix
+++ modules/home/features/desktop/theme/default.nix
@@ -1,21 +1,29 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  options,
+  ...
+}:
 let
+  hasStylix = options ? stylix;
 in
 {
   imports = [
     ../../../../common/theme
   ];
 
-  stylix = {
-    icons.enable = true;
-    targets = {
-      nixcord.enable = false;
-      zen-browser.profileNames = [ "default" ];
-    };
-    cursor = {
-      name = "BreezeX-RosePine-Linux";
-      package = pkgs.rose-pine-cursor;
-      size = 16;
+  config = lib.optionalAttrs hasStylix {
+    stylix = {
+      icons.enable = true;
+      targets = {
+        nixcord.enable = false;
+        zen-browser.profileNames = [ "default" ];
+      };
+      cursor = {
+        name = "BreezeX-RosePine-Linux";
+        package = pkgs.rose-pine-cursor;
+        size = 16;
+      };
     };
   };
 }
