4b7c29f2b38263115946414b4f810e28be5469d3
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-01 16:06:03 +0000

    chore(flake): use gemini-cli bin and major update

--- modules/home/features/cli/gemini.nix
+++ modules/home/features/cli/gemini.nix
@@ -1,12 +1,16 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
   options.features.cli.gemini.enable = lib.mkEnableOption "gemini-cli";
 
   config = lib.mkIf config.features.cli.gemini.enable {
-    programs.gemini-cli.enable = true;
+    programs.gemini-cli = {
+      enable = true;
+      package = pkgs.gemini-cli-bin;
+    };
   };
 }
