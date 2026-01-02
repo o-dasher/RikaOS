1e48cc01d6fa70680161aa420e77c30806eaa4ac
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-02 09:13:14 -0400

    Launch browser on second workspace always

--- modules/home/profiles/browser.nix
+++ modules/home/profiles/browser.nix
@@ -8,9 +8,14 @@
 {
   options.profiles.browser = {
     enable = lib.mkEnableOption "browser profile";
+    package = lib.mkOption {
+      type = lib.types.unspecified;
+      description = "browser package";
+    };
   };
 
   config = lib.mkIf config.profiles.browser.enable {
+    profiles.browser.package = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
     programs.zen-browser = {
       enable = true;
       profiles.default.extensions.packages =
