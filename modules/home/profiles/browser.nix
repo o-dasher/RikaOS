937936255e1ebf3986352b0da9b8d5fb071430d1
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-02 18:58:08 +0000

    feat: yeyy better zen browser

--- modules/home/profiles/browser.nix
+++ modules/home/profiles/browser.nix
@@ -15,9 +15,27 @@
   };
 
   config = lib.mkIf config.profiles.browser.enable {
-    profiles.browser.package = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    profiles.browser.package = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.twilight;
     programs.zen-browser = {
       enable = true;
+      package = config.profiles.browser.package;
+      policies = {
+        AutofillAddressEnabled = true;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+      };
       profiles.default.extensions.packages =
         with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
           ublock-origin
