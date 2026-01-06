47be2f553f8409ac8b061bd6eee33709d1dd8849
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-06 15:56:38 -0400

    NixGL is no longer a requirement :D

--- modules/home/profiles/generic-linux.nix
+++ modules/home/profiles/generic-linux.nix
@@ -2,7 +2,6 @@
   lib,
   config,
   pkgs,
-  inputs,
   ...
 }:
 {
@@ -13,10 +12,5 @@
     # Fixes PAM on generic linux desktops.
     pamShim.enable = true;
     programs.hyprlock.package = config.lib.pamShim.replacePam pkgs.hyprlock;
-
-    # NixGL configuration
-    nixGL.packages = inputs.nixgl.packages;
-    programs.ghostty.package = config.lib.nixGL.wrap pkgs.ghostty;
-    wayland.windowManager.hyprland.package = lib.mkForce (config.lib.nixGL.wrap pkgs.hyprland);
   };
 }
