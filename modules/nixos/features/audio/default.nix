0289809401f59c2c010c696a74f3c5b476aeda7d
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-06 16:40:20 +0000

    refactor(audio): simplify pipewire configuration and sample rate

--- modules/nixos/features/audio/default.nix
+++ modules/nixos/features/audio/default.nix
@@ -8,46 +8,45 @@
 
   config = lib.mkIf config.features.audio.enable {
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      lowLatency = {
-        enable = true;
+    services.pipewire =
+      let
         rate = 192000;
-      };
-      extraConfig = {
-        pipewire-pulse."99-z-audiophile"."stream.properties"."resample.quality" = 14;
-        client."99-z-audiophile"."stream.properties"."resample.quality" = 14;
-        pipewire."99-z-audiophile"."context.properties" = {
-          "default.clock.rate" = 192000;
-          "default.clock.allowed-rates" = [
-            44100
-            48000
-            88200
-            96000
-            176400
-            192000
-            352800
-            384000
-          ];
+      in
+      {
+        enable = true;
+        pulse.enable = true;
+        jack.enable = true;
+        wireplumber.enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
+        lowLatency = {
+          inherit rate;
+          enable = true;
+          alsa = {
+            enable = true;
+            devicePattern = "~alsa_output.*USB_Audio.*";
+            format = "S32LE";
+          };
+        };
+        extraConfig = {
+          pipewire-pulse."99-z-defaults"."stream.properties"."resample.quality" = 14;
+          client."99-z-defaults"."stream.properties"."resample.quality" = 14;
+          pipewire."99-z-defaults"."context.properties" = {
+            "default.clock.rate" = rate;
+            "default.clock.allowed-rates" = [
+              44100
+              48000
+              88200
+              96000
+              176400
+              192000
+              352800
+              384000
+            ];
+          };
         };
       };
-      wireplumber.extraConfig = {
-        "99-z-kinera-celest" = {
-          "monitor.alsa.rules" = [
-            {
-              matches = [ { "node.name" = "~alsa_output.*USB_Audio.*"; } ];
-              actions.update-props."audio.format" = "S32LE";
-            }
-          ];
-        };
-      };
-    };
   };
 }
