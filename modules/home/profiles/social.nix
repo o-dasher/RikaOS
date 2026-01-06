01379e22d75c5af62ce3e61096a82f1bacf2a3bf
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-06 23:48:22 +0000

    refactor(social/discord): extract discord configuration into dedicated feature module

--- modules/home/profiles/social.nix
+++ modules/home/profiles/social.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 {
@@ -9,50 +8,7 @@
     enable = lib.mkEnableOption "social profile";
   };
 
-  config =
-    let
-      krisp-patcher = pkgs.fetchurl {
-        url = "https://github.com/keysmashes/sys/raw/25f9bc04e6b8d59c1abb32bf4e7ce8ed8de048e2/hm/discord/krisp-patcher.py";
-        hash = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
-      };
-      pythonWithPackages = pkgs.python3.withPackages (
-        p: with p; [
-          capstone
-          pyelftools
-        ]
-      );
-      patcherScript = pkgs.writeShellScript "krisp-patcher-wrapper" ''
-        ${pkgs.fd}/bin/fd '^discord_krisp.node$' "$HOME/.config/discord" \
-          --exec ${pythonWithPackages}/bin/python ${krisp-patcher}
-      '';
-    in
-    lib.mkIf config.profiles.social.enable {
-      systemd.user.services.krisp-patcher = {
-        Unit = {
-          Description = "Patch Discord Krisp node";
-          After = [ "graphical-session-pre.target" ];
-          PartOf = [ "graphical-session.target" ];
-        };
-
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
-
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${patcherScript}";
-        };
-      };
-
-      programs.nixcord = {
-        enable = true;
-        discord.enable = true;
-        vesktop.enable = true;
-        config.plugins = {
-          fakeNitro.enable = true;
-          youtubeAdblock.enable = true;
-          webScreenShareFixes.enable = true;
-        };
-      };
-    };
+  config = lib.mkIf config.profiles.social.enable {
+    features.social.discord.enable = true;
+  };
 }
