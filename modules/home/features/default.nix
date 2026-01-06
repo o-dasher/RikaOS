78224b885084d8434ae78fc8b139ada95364b0ab
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-06 19:48:22 -0400

    Reorganize discord stuff

--- modules/home/features/default.nix
+++ modules/home/features/default.nix
@@ -6,6 +6,7 @@
     ./editors
     ./dev
     ./gaming
+    ./social
     ./terminal
   ];
 }
---
+++ modules/home/features/social/discord.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.features.social.discord;
+
+  krisp-patcher = pkgs.fetchurl {
+    url = "https://github.com/keysmashes/sys/raw/25f9bc04e6b8d59c1abb32bf4e7ce8ed8de048e2/hm/discord/krisp-patcher.py";
+    hash = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
+  };
+
+  pythonWithPackages = pkgs.python3.withPackages (
+    p: with p; [
+      capstone
+      pyelftools
+    ]
+  );
+
+  patcherScript = pkgs.writeShellScript "krisp-patcher-wrapper" ''
+    ${pkgs.fd}/bin/fd '^discord_krisp.node$' "$HOME/.config/discord" \
+      --exec ${pythonWithPackages}/bin/python ${krisp-patcher}
+  '';
+in
+{
+  options.features.social.discord = {
+    enable = lib.mkEnableOption "Discord with Krisp";
+    enableKrispPatch = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable the Krisp noise suppression patch for Discord";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.user.services.krisp-patcher = lib.mkIf cfg.enableKrispPatch {
+      Unit = {
+        Description = "Patch Discord Krisp node";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${patcherScript}";
+      };
+    };
+
+    programs.nixcord = {
+      enable = true;
+      vesktop.enable = true;
+      config.plugins = {
+        fakeNitro.enable = true;
+        youtubeAdblock.enable = true;
+        webScreenShareFixes.enable = true;
+      };
+    };
+  };
+}
