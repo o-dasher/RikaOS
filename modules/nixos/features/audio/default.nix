ee4ca5c9ad76543bddf1343a8ac349826d5047fc
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-06 12:47:32 -0400

    Forgot the default rate

--- modules/nixos/features/audio/default.nix
+++ modules/nixos/features/audio/default.nix
@@ -8,38 +8,45 @@
 
   config = lib.mkIf config.features.audio.enable {
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      lowLatency = {
-        enable = true;
+    services.pipewire =
+      let
         rate = 192000;
+      in
+      {
+        enable = true;
+        pulse.enable = true;
+        jack.enable = true;
+        wireplumber.enable = true;
         alsa = {
           enable = true;
-          devicePattern = "~alsa_output.*USB_Audio.*";
-          format = "S32LE";
+          support32Bit = true;
+        };
+        lowLatency = {
+          inherit rate;
+          enable = true;
+          alsa = {
+            enable = true;
+            devicePattern = "~alsa_output.*USB_Audio.*";
+            format = "S32LE";
+          };
+        };
+        extraConfig = {
+          pipewire-pulse."99-z-defaults"."stream.properties"."resample.quality" = 14;
+          client."99-z-defaults"."stream.properties"."resample.quality" = 14;
+          pipewire."99-z-defaults"."context.properties" = {
+            "default.clock.rate" = rate;
+            "default.clock.allowed-rates" = [
+              44100
+              48000
+              88200
+              96000
+              176400
+              192000
+              352800
+              384000
+            ];
+          };
         };
       };
-      extraConfig = {
-        pipewire-pulse."99-z-defaults"."stream.properties"."resample.quality" = 14;
-        client."99-z-defaults"."stream.properties"."resample.quality" = 14;
-        pipewire."99-z-defaults"."context.properties"."default.clock.allowed-rates" = [
-          44100
-          48000
-          88200
-          96000
-          176400
-          192000
-          352800
-          384000
-        ];
-      };
-    };
   };
 }
