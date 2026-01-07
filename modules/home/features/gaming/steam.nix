931c4d698a9a8ebfddfce0abfefb0693a5ec4858
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-07 21:24:05 +0000

    refactor(gaming): simplify autostart logic

--- modules/home/features/gaming/steam.nix
+++ modules/home/features/gaming/steam.nix
@@ -11,20 +11,6 @@ in
   options.features.gaming.steam.enable = lib.mkEnableOption "Steam";
 
   config = lib.mkIf (config.features.gaming.enable && cfg.enable) {
-    systemd.user.services.steam = {
-      Unit = {
-        Description = "Steam Client";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-      Service = {
-        ExecStart = "${lib.getExe pkgs.steam} -silent";
-        Restart = "on-failure";
-        Slice = "app-graphical.slice";
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
+    systemd.user.services.steam = config.rika.utils.mkAutostartService "${lib.getExe pkgs.steam} -silent";
   };
 }
