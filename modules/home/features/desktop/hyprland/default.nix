1ba7bc7f1d14658e8f7e2f4abc88e091c95f6bf3
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-10 17:16:35 -0400

    ntsync and fix

--- modules/home/features/desktop/hyprland/default.nix
+++ modules/home/features/desktop/hyprland/default.nix
@@ -92,7 +92,7 @@
             "tag +games, match:content game"
             "tag +games, match:class ^(steam_app_.*|gamescope|osu!)$"
 
-            "tag +floaty, match:class ^(.blueman-manager-wrapped|Thunar|com.github.wwmm.easyeffects|com.saivert.pwvucontrol)$"
+            "tag +floaty, match:class ^(.blueman-manager-wrapped|thunar|com.github.wwmm.easyeffects|com.saivert.pwvucontrol)$"
 
             "match:tag games, sync_fullscreen on, no_shadow on, no_blur on, no_anim on, immediate on"
             "match:tag floaty, float on, center on, size (monitor_w*0.6) (monitor_h*0.6)"
