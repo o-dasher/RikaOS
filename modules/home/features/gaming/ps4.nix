916c6b9a6c6eaa6bca4dfefc8fc9c90b3821d29a
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-13 21:05:51 +0000

    feat(gaming): using flatpak shadps4

--- modules/home/features/gaming/ps4.nix
+++ modules/home/features/gaming/ps4.nix
@@ -1,15 +1,12 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 {
   options.features.gaming.ps4.enable = lib.mkEnableOption "ps4";
 
   config = lib.mkIf (config.features.gaming.enable && config.features.gaming.ps4.enable) {
-    home.packages = [
-      pkgs.shadps4
-    ];
+    services.flatpak.packages = [ "net.shadps4.shadPS4" ];
   };
 }
