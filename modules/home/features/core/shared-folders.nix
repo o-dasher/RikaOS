fa286b2c300caa720f906c9410f8f186d9162339
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-14 23:59:55 +0000

    feat(hosts/hinamizawa): well well well

--- modules/home/features/core/shared-folders.nix
+++ modules/home/features/core/shared-folders.nix
@@ -1,9 +1,7 @@
 { lib, ... }:
 {
   options.features.filesystem.sharedFolders = with lib; {
-    enable = (mkEnableOption "sharedFolders") // {
-      default = true;
-    };
+    enable = mkEnableOption "sharedFolders";
     configurationRoot = mkOption {
       default = "/shared/.config";
       type = types.str;
