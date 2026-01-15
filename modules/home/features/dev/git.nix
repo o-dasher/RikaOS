00ebf552b2dea4e62b6f1e0fcde81a20f8ac6b8f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-15 12:16:57 -0400

    Meh no gh

--- modules/home/features/dev/git.nix
+++ modules/home/features/dev/git.nix
@@ -7,7 +7,6 @@
   options.features.dev.git.enable = lib.mkEnableOption "git";
 
   config = lib.mkIf config.features.dev.git.enable {
-    programs.gh.enable = true;
     programs.git.enable = true;
   };
 }
