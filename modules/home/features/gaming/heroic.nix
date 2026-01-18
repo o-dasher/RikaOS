0ce0a4eb3e20441c5b70873275dd91d57fae0029
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-18 15:19:20 -0400

    Run heroic at background so instant startup + game updates

--- modules/home/features/gaming/heroic.nix
+++ modules/home/features/gaming/heroic.nix
@@ -8,6 +8,7 @@
   options.features.gaming.heroic.enable = lib.mkEnableOption "heroic";
 
   config = lib.mkIf (config.features.gaming.enable && config.features.gaming.heroic.enable) {
+    systemd.user.services.heroic = config.rika.utils.mkAutostartService "${lib.getExe pkgs.heroic}";
     home.packages = [
       pkgs.heroic
     ];
