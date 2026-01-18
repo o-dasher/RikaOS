b6783301c384c21546998bc2d07725d939414b71
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-18 17:32:44 +0000

    feat(desktop/hyprland): no more hyprland cache

--- flake.nix
+++ flake.nix
@@ -192,11 +192,9 @@
           "https://hercules-ci.cachix.org"
           "https://walker.cachix.org"
           "https://walker-git.cachix.org"
-          "https://hyprland.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
