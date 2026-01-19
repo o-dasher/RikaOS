0a0042cc04be399b59673717806ed6c8bd1d8f23
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-18 20:53:13 -0400

    Add zapzap client

--- modules/home/profiles/social.nix
+++ modules/home/profiles/social.nix
@@ -10,5 +10,22 @@
 
   config = lib.mkIf config.profiles.social.enable {
     features.social.discord.enable = true;
+    programs.zapzap = {
+      enable = true;
+      settings = {
+        web.scroll_animator = true;
+        storage-whats.notification = false;
+        notification = {
+          app = false;
+          donation_message = true;
+        };
+        system = {
+          start_background = true;
+          start_system = true;
+          wayland = true;
+          notificationCounter = true;
+        };
+      };
+    };
   };
 }
