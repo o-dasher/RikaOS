4f2be1ab3a3d01ffa716cee3a0046a85881e0252
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-21 01:15:01 +0000

    chore(gaming/minecraft): update paper server and geyser plugins

--- hosts/gensokyo/configuration.nix
+++ hosts/gensokyo/configuration.nix
@@ -61,35 +61,25 @@ in
         serverProperties = {
           server-port = javaPort;
           server-ip = "::";
-          motd = "Gensokyo Minecraft Server";
+          motd = "Gensokyo Survival";
           max-players = 10;
           difficulty = "hard";
           gamemode = "survival";
-          online-mode = true;
+          online-mode = false;
           spawn-protection = 0;
           view-distance = 12;
           simulation-distance = 10;
         };
 
         symlinks = {
-          plugins = pkgs.linkFarmFromDrvs "plugins" (
-            builtins.attrValues {
-              Geyser = pkgs.fetchurl {
-                url = "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot";
-                hash = "sha256-YbhG+b6NGL487yMRQvAt8gdNylvjrGeF0e9ffC5OR40=";
-                name = "Geyser-Spigot.jar";
-              };
-              Floodgate = pkgs.fetchurl {
-                url = "https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot";
-                hash = "sha256-1kevbNh1zsZbJj/+TlEgTabptu24tIHTe6/czILxBdk=";
-                name = "Floodgate-Spigot.jar";
-              };
-              AutoReloader = pkgs.fetchurl {
-                url = "https://github.com/notTamion/AutoReloader/releases/download/v1.0.1/AutoReloader-1.0.1.jar";
-                hash = "sha256-EV7os5smIld9/mvQfCNj2QF+IEMSIwMRGBAjPzN50p4=";
-              };
-            }
-          );
+          "plugins/Geyser-Spigot.jar" = pkgs.fetchurl {
+            url = "https://download.geysermc.org/v2/projects/geyser/versions/2.4.2/builds/latest/downloads/spigot";
+            hash = "sha256-aelVbofbyrA9N6Yoeqqb8y5aQfS7uinKluk93QDHpkE=";
+          };
+          "plugins/Floodgate-Spigot.jar" = pkgs.fetchurl {
+            url = "https://download.geysermc.org/v2/projects/floodgate/versions/2.2.3/builds/latest/downloads/spigot";
+            hash = "sha256-aMrZ5yrqy6LWfYYEeh3mR3nIDS4g5SOTPJNSdgWieOY=";
+          };
         };
 
         files."plugins/Geyser-Spigot/config.yml" = {
