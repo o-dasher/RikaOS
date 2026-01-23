4c17ba4a76da6dba5169e9e1f3c25d4afab0bea7
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-23 11:54:21 +0000

    fix(audio): fix audio crackling

--- modules/nixos/features/audio/default.nix
+++ modules/nixos/features/audio/default.nix
@@ -8,34 +8,31 @@
 
   config = lib.mkIf config.features.audio.enable {
     security.rtkit.enable = true;
-    services.pipewire =
-      let
-      in
-      {
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+      alsa = {
         enable = true;
-        pulse.enable = true;
-        jack.enable = true;
-        wireplumber.enable = true;
-        alsa = {
-          enable = true;
-          support32Bit = true;
-        };
-        extraConfig = {
-          pipewire-pulse."99-z-defaults"."stream.properties"."resample.quality" = 14;
-          client."99-z-defaults"."stream.properties"."resample.quality" = 14;
-          pipewire."99-z-defaults"."context.properties" = {
-            "default.clock.allowed-rates" = [
-              44100
-              48000
-              88200
-              96000
-              176400
-              192000
-              352800
-              384000
-            ];
-          };
+        support32Bit = true;
+      };
+      extraConfig = {
+        pipewire-pulse."99-z-defaults"."stream.properties"."resample.quality" = 10;
+        client."99-z-defaults"."stream.properties"."resample.quality" = 10;
+        pipewire."99-z-defaults"."context.properties" = {
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            176400
+            192000
+            352800
+            384000
+          ];
         };
       };
+    };
   };
 }
