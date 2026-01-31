d78cbf113ba4eadbb7db924e6330937b62a79959
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-01-31 04:57:25 +0000

    feat(networking): set up sftpgo service and bindings

--- hosts/gensokyo/configuration.nix
+++ hosts/gensokyo/configuration.nix
@@ -18,9 +18,9 @@
       enable = true;
       rootFolderNames = [
         "/shared"
-        "/shared/Media"
       ];
       folderNames = [
+        "/shared/Media"
         "/shared/Media/Music"
         "/shared/Media/Movies"
         "/shared/Media/Series"
@@ -114,12 +114,24 @@
       };
     };
     jellyfin.enable = true;
-    filebrowser = {
+    sftpgo = {
       enable = true;
+      extraReadWriteDirs = [ "/shared/Media" ];
       settings = {
-        port = 8080;
-        address = "127.0.0.1";
-        root = "/shared/Media";
+        sftpd.bindings = [
+          {
+            address = "";
+            port = 2022;
+          }
+        ];
+        httpd.bindings = [
+          {
+            port = 8080;
+            address = "127.0.0.1";
+            enable_web_admin = true;
+            enable_web_client = true;
+          }
+        ];
       };
     };
     nginx = {
@@ -128,23 +140,25 @@
       recommendedTlsSettings = true;
       recommendedOptimisation = true;
       recommendedGzipSettings = true;
-      virtualHosts."jellyfin.dshs.cc" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8096";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_buffering off;
-          '';
+      virtualHosts = {
+        "jellyfin.dshs.cc" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:8096";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_buffering off;
+            '';
+          };
         };
-      };
-      virtualHosts."files.dshs.cc" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
-          proxyWebsockets = true;
+        "files.dshs.cc" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:8080";
+            proxyWebsockets = true;
+          };
         };
       };
     };
@@ -158,10 +172,11 @@
   networking.firewall.allowedTCPPorts = [
     80
     443
+    2022
   ];
 
   users.users = {
-    filebrowser.extraGroups = [ "users" ];
+    sftpgo.extraGroups = [ "users" ];
     jellyfin.extraGroups = [ "users" ];
     thiago = {
       isNormalUser = true;
