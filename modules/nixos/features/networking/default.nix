3c02fef08dc26362fe265edf61ebf49f00dbed1d
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-05 18:14:10 +0000

    fix(networking): fix networking

--- modules/nixos/features/networking/default.nix
+++ modules/nixos/features/networking/default.nix
@@ -11,7 +11,6 @@ in
 with lib;
 {
   options.features.networking = {
-    networkManager.enable = mkEnableOption "NetworkManager";
     cloudflare = {
       warp.enable = mkEnableOption "Warp";
       dns.enable = mkEnableOption "DNS";
@@ -38,7 +37,8 @@ with lib;
 
   config = mkIf modCfg.enable (mkMerge [
     {
-      networking.networkmanager.enable = modCfg.networkManager.enable;
+      networking.useNetworkd = true;
+      systemd.network.enable = true;
       services.cloudflare-warp.enable = modCfg.cloudflare.warp.enable;
     }
 
@@ -50,40 +50,35 @@ with lib;
           FallbackDNS = [ ];
         };
       };
-      systemd.services.cloudflared-doh = {
-        description = "Cloudflare DNS over HTTPS proxy";
-        after = [ "network-online.target" ];
-        wants = [ "network-online.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          ExecStart = "${lib.getExe pkgs.cloudflared} proxy-dns --port 5053";
-          Restart = "on-failure";
-          DynamicUser = true;
+      systemd = {
+        services.cloudflared-doh = {
+          description = "Cloudflare DNS over HTTPS proxy";
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            ExecStart = "${lib.getExe pkgs.cloudflared} proxy-dns --port 5053";
+            Restart = "on-failure";
+            DynamicUser = true;
+          };
+        };
+        network.networks."99-cloudflare-dns" = {
+          matchConfig.Name = "*";
+          dhcpV4Config.UseDNS = false;
+          ipv6AcceptRAConfig.UseDNS = false;
         };
       };
     })
 
     (mkIf stableIPv6Cfg.enable {
-      networking = {
-        useDHCP = false;
-        useNetworkd = true;
-      };
-      systemd.network = {
-        enable = true;
-        networks.${stableIPv6Cfg.interface} = {
-          matchConfig.Name = stableIPv6Cfg.matchInterface;
-          address = [ "${stableIPv6Cfg.ipv6}/64" ];
-          networkConfig = {
-            DHCP = "ipv4";
-            IPv6AcceptRA = true;
-            IPv6LinkLocalAddressGenerationMode = "stable-privacy";
-          };
-          dhcpV4Config = mkIf modCfg.cloudflare.dns.enable {
-            UseDNS = false;
-          };
-          ipv6AcceptRAConfig = mkIf modCfg.cloudflare.dns.enable {
-            UseDNS = false;
-          };
+      networking.useDHCP = false;
+      systemd.network.networks.${stableIPv6Cfg.interface} = {
+        matchConfig.Name = stableIPv6Cfg.matchInterface;
+        address = [ "${stableIPv6Cfg.ipv6}/64" ];
+        networkConfig = {
+          DHCP = "ipv4";
+          IPv6AcceptRA = true;
+          IPv6LinkLocalAddressGenerationMode = "stable-privacy";
         };
       };
     })
