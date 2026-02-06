63d98e03b44a1642d7125d4640319306cdbfd6bf
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-06 08:06:21 -0400

    Setup github copilot

--- modules/home/features/development/git.nix
+++ modules/home/features/development/git.nix
@@ -14,12 +14,11 @@ with lib;
     programs = {
       ssh = {
         enable = true;
-        matchBlocks = {
-          "github.com" = {
-            hostname = "github.com";
-            user = "git";
-            identityFile = "~/.ssh/id_ed25519";
-          };
+        enableDefaultConfig = false;
+        matchBlocks."github.com" = {
+          hostname = "github.com";
+          user = "git";
+          identityFile = "~/.ssh/id_ed25519";
         };
       };
       git = {
