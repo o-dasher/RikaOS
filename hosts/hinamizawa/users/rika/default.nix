d7ba2122e83211b3d026d7940dd4505a3674d3cd
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-06 00:25:30 +0000

    feat(hosts/hinamizawa): mount server on native filesystem

--- hosts/hinamizawa/users/rika/default.nix
+++ hosts/hinamizawa/users/rika/default.nix
@@ -1,5 +1,7 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
 {
@@ -39,10 +41,32 @@
 
     # tools
     pwvucontrol
+    sshfs
   ];
 
   programs = {
     home-manager.enable = true;
     imv.enable = true;
+    sftpman = {
+      enable = true;
+      defaultSshKey = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      mounts."gensokyo" = {
+        host = "files.dshs.cc";
+        user = "thiago";
+        mountPoint = "/shared/Media";
+        mountDestPath = "${config.home.homeDirectory}/mnt/gensokyo";
+        authType = "publickey";
+        mountOptions = [
+          "reconnect"
+          "ServerAliveInterval=15"
+          "ServerAliveCountMax=3"
+          "ConnectTimeout=5"
+          "default_permissions"
+          "nodev"
+          "nosuid"
+          "noexec"
+        ];
+      };
+    };
   };
 }
