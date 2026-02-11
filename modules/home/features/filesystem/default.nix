3bf6aa7f0d082b759c6ac0a3114c45d1db379f04
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-10 21:46:09 -0400

    Remove sftpman

--- modules/home/features/filesystem/default.nix
+++ modules/home/features/filesystem/default.nix
@@ -1,59 +1,7 @@
-{ lib, config, ... }:
+{ lib, ... }:
 with lib;
 {
-  imports = [
-    ./sftpman.nix
-  ];
-
   options.features.filesystem = {
-    sftpman = {
-      automount.enable = mkEnableOption "sftpman automount via systemd";
-      defaultSSHKey = mkOption {
-        type = types.str;
-        default = "${config.home.homeDirectory}/.ssh/id_ed25519";
-      };
-      mounts = mkOption {
-        default = { };
-        description = "sftpman mounts with sensible defaults";
-        type = types.attrsOf (
-          types.submodule (
-            { name, ... }:
-            {
-              options = {
-                host = mkOption { type = types.str; };
-                user = mkOption { type = types.str; };
-                mountPoint = mkOption { type = types.str; };
-                mountDestPath = mkOption {
-                  type = types.str;
-                  default = "${config.home.homeDirectory}/mnt/${name}";
-                };
-                authType = mkOption {
-                  type = types.str;
-                  default = "publickey";
-                };
-                sshKey = mkOption {
-                  type = types.nullOr types.str;
-                  default = config.features.filesystem.sftpman.defaultSSHKey;
-                };
-                mountOptions = mkOption {
-                  type = types.listOf types.str;
-                  default = [
-                    "reconnect"
-                    "ServerAliveInterval=15"
-                    "ServerAliveCountMax=3"
-                    "ConnectTimeout=5"
-                    "default_permissions"
-                    "nodev"
-                    "nosuid"
-                    "noexec"
-                  ];
-                };
-              };
-            }
-          )
-        );
-      };
-    };
     enable = mkEnableOption "filesystem features" // {
       default = true;
     };
