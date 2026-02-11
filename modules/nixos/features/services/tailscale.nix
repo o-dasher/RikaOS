7084f6298c64b1c0c3df5f3a6c6e0fb36d3a8088
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-11 07:12:08 +0000

    refactor(networking): tailscale refactor

--- modules/nixos/features/services/tailscale.nix
+++ modules/nixos/features/services/tailscale.nix
@@ -9,39 +9,37 @@ let
 in
 with lib;
 {
-  config = mkIf (modCfg.enable && cfg.enable && config.age.secrets ? tailscale-auth-key) (mkMerge [
-    {
-      services.tailscale = {
-        enable = true;
-        authKeyFile = config.age.secrets.tailscale-auth-key.path;
-        extraUpFlags = [
-          "--accept-dns=true"
-          "--hostname=${config.networking.hostName}"
-        ];
-      };
-    }
-    (mkIf cfg.dnsFirewall.enable {
-      networking.firewall.interfaces."tailscale0" = {
-        allowedTCPPorts = [ 53 ];
-        allowedUDPPorts = [ 53 ];
-      };
-    })
-    (mkIf cfg.dnsServer.enable {
-      services.coredns = {
-        enable = true;
-        config = ''
-          ${cfg.dnsServer.zone}:53 {
-            errors
-            log
-            bind ${cfg.dnsServer.tailnetIP}
-            hosts {
-              ${cfg.dnsServer.tailnetIP} ${lib.concatStringsSep " " cfg.dnsServer.hosts}
-              fallthrough
+  config =
+    let
+      port = 53;
+    in
+    mkIf (modCfg.enable && cfg.enable && config.age.secrets ? tailscale-auth-key) (mkMerge [
+      {
+        services.tailscale = {
+          enable = true;
+          authKeyFile = config.age.secrets.tailscale-auth-key.path;
+          extraUpFlags = [
+            "--accept-dns=true"
+            "--hostname=${config.networking.hostName}"
+          ];
+        };
+      }
+      (mkIf cfg.dns.server.enable {
+        services.coredns = {
+          enable = true;
+          config = ''
+            ${cfg.dns.server.zone}:${port}{
+              errors
+              log
+              bind ${cfg.dns.server.tailnetIP}
+              hosts {
+                ${cfg.dns.server.tailnetIP} ${lib.concatStringsSep " " cfg.dns.server.hosts}
+                fallthrough
+              }
+              forward . ${lib.concatStringsSep " " cfg.dns.server.forwarders}
             }
-            forward . ${lib.concatStringsSep " " cfg.dnsServer.forwarders}
-          }
-        '';
-      };
-    })
-  ]);
+          '';
+        };
+      })
+    ]);
 }
