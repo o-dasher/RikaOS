7cf83113c013a1f80023c698cfe10552d6205606
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-11 03:16:07 -0400

    Remove prowlarr custom indexer

--- modules/nixos/features/services/tailscale.nix
+++ modules/nixos/features/services/tailscale.nix
@@ -9,37 +9,33 @@ let
 in
 with lib;
 {
-  config =
-    let
-      port = 53;
-    in
-    mkIf (modCfg.enable && cfg.enable && config.age.secrets ? tailscale-auth-key) (mkMerge [
-      {
-        services.tailscale = {
-          enable = true;
-          authKeyFile = config.age.secrets.tailscale-auth-key.path;
-          extraUpFlags = [
-            "--accept-dns=true"
-            "--hostname=${config.networking.hostName}"
-          ];
-        };
-      }
-      (mkIf cfg.dns.server.enable {
-        services.coredns = {
-          enable = true;
-          config = ''
-            ${cfg.dns.server.zone}:${port}{
-              errors
-              log
-              bind ${cfg.dns.server.tailnetIP}
-              hosts {
-                ${cfg.dns.server.tailnetIP} ${lib.concatStringsSep " " cfg.dns.server.hosts}
-                fallthrough
-              }
-              forward . ${lib.concatStringsSep " " cfg.dns.server.forwarders}
+  config = mkIf (modCfg.enable && cfg.enable && config.age.secrets ? tailscale-auth-key) (mkMerge [
+    {
+      services.tailscale = {
+        enable = true;
+        authKeyFile = config.age.secrets.tailscale-auth-key.path;
+        extraUpFlags = [
+          "--accept-dns=true"
+          "--hostname=${config.networking.hostName}"
+        ];
+      };
+    }
+    (mkIf cfg.dns.server.enable {
+      services.coredns = {
+        enable = true;
+        config = ''
+          ${cfg.dns.server.zone}:53 {
+            errors
+            log
+            bind ${cfg.dns.server.tailnetIP}
+            hosts {
+              ${cfg.dns.server.tailnetIP} ${lib.concatStringsSep " " cfg.dns.server.hosts}
+              fallthrough
             }
-          '';
-        };
-      })
-    ]);
+            forward . ${lib.concatStringsSep " " cfg.dns.server.forwarders}
+          }
+        '';
+      };
+    })
+  ]);
 }
