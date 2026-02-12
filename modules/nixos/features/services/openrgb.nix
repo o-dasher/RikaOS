2f74073f95ea9f6fc3f4ea3a06a639097f8b80ba
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-12 00:49:41 +0000

    feat(hardware/openrgb): make openrgb wake up after suspend

--- modules/nixos/features/services/openrgb.nix
+++ modules/nixos/features/services/openrgb.nix
@@ -1,21 +1,35 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
 let
   modCfg = config.features.services;
   cfg = modCfg.openrgb;
+
+  openrgbProfile = "/var/lib/OpenRGB/${config.services.hardware.openrgb.startupProfile}";
+  openrgbReloadCmd = "${pkgs.openrgb}/bin/openrgb --profile ${openrgbProfile}";
 in
 with lib;
 {
   config = mkIf (modCfg.enable && cfg.enable) {
-    services.hardware.openrgb.enable = true;
-    services.hardware.openrgb.startupProfile = "black.orp";
+    services.hardware.openrgb = {
+      enable = true;
+      startupProfile = "black.orp";
+    };
 
-    systemd.tmpfiles.rules = [
-      "d /var/lib/OpenRGB 0755 root root -"
-      "L+ /var/lib/OpenRGB/black.orp - - - - ${../../../../assets/OpenRGB/black.orp}"
-    ];
+    systemd = {
+      tmpfiles.rules = [
+        "d /var/lib/OpenRGB 0755 root root -"
+        "L+ /var/lib/OpenRGB/black.orp - - - - ${../../../../assets/OpenRGB/black.orp}"
+      ];
+      services = {
+        systemd-suspend.serviceConfig.ExecStartPost = lib.mkAfter [ openrgbReloadCmd ];
+        systemd-hibernate.serviceConfig.ExecStartPost = lib.mkAfter [ openrgbReloadCmd ];
+        systemd-hybrid-sleep.serviceConfig.ExecStartPost = lib.mkAfter [ openrgbReloadCmd ];
+        systemd-suspend-then-hibernate.serviceConfig.ExecStartPost = lib.mkAfter [ openrgbReloadCmd ];
+      };
+    };
   };
 }
