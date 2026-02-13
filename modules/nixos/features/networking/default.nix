cc200b5bba879d1b69920a570b357be07bf9cd55
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-13 07:04:10 +0000

    fix(networking): fix build failure

--- modules/nixos/features/networking/default.nix
+++ modules/nixos/features/networking/default.nix
@@ -18,24 +18,22 @@ with lib;
     privacyIPv6.enable = mkEnableOption "Privacy IPv6 address generation";
   };
 
-  config = mkMerge [
-    (mkIf (cfg.enable && cfg.primaryInterface != null) {
-      networking.useNetworkd = true;
-      systemd.network = {
-        enable = true;
-        networks."99-network" = mkMerge [
-          (mkIf cfg.privacyIPv6.enable {
-            networkConfig = {
-              IPv6LinkLocalAddressGenerationMode = "stable-privacy";
-              IPv6PrivacyExtensions = "yes";
-            };
-          })
-          (mkIf cfg.cloudflare.dns.enable {
-            dhcpV4Config.UseDNS = false;
-            ipv6AcceptRAConfig.UseDNS = false;
-          })
-        ];
-      };
-    })
-  ];
+  config = mkIf cfg.enable {
+    networking.useNetworkd = true;
+    systemd.network = {
+      enable = true;
+      networks."99-network" = mkMerge [
+        (mkIf cfg.privacyIPv6.enable {
+          networkConfig = {
+            IPv6LinkLocalAddressGenerationMode = "stable-privacy";
+            IPv6PrivacyExtensions = "yes";
+          };
+        })
+        (mkIf cfg.cloudflare.dns.enable {
+          dhcpV4Config.UseDNS = false;
+          ipv6AcceptRAConfig.UseDNS = false;
+        })
+      ];
+    };
+  };
 }
