9431cb36709f887b85e6310d4dcec983b852843f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-21 04:08:53 -0400

    Using mkmerge syntax to simplify stuff

--- hosts/wired/configuration.nix
+++ hosts/wired/configuration.nix
@@ -128,105 +128,109 @@ in
         group = "headscale";
       });
 
-  services = {
-    fail2ban = {
-      enable = true;
-      bantime = "24h";
-    };
-    keycloak = lib.mkIf config.rika.utils.hasSecrets {
-      enable = true;
-      database.passwordFile = config.age.secrets.keycloak-db-password.path;
-      settings = {
-        http-enabled = true;
-        http-host = "127.0.0.1";
-        http-port = 8081;
-        proxy-headers = "xforwarded";
-        hostname = keycloakDomain;
+  services = lib.mkMerge [
+    {
+      fail2ban = {
+        enable = true;
+        bantime = "24h";
       };
-    };
-    headscale = lib.mkIf config.rika.utils.hasSecrets {
-      enable = true;
-      address = "127.0.0.1";
-      settings = {
-        server_url = headscaleURI;
-        policy.mode = "database";
-        dns = {
-          magic_dns = true;
-          base_domain = "wired.local";
-          search_domains = [ ];
-          override_local_dns = false;
-          extra_records = [ ];
-          nameservers = {
-            split = { };
-            global = [
-              "1.1.1.1"
-              "1.0.0.1"
+      openssh = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+        };
+      };
+      caddy = {
+        enable = true;
+        openFirewall = true;
+        virtualHosts = {
+          ${headscaleDomain}.extraConfig = ''
+            handle /admin* {
+              reverse_proxy 127.0.0.1:${toString config.services.headplane.settings.server.port}
+            }
+
+            reverse_proxy 127.0.0.1:${toString config.services.headscale.port}
+          '';
+          ${keycloakDomain}.extraConfig = ''
+            reverse_proxy 127.0.0.1:${toString config.services.keycloak.settings.http-port}
+          '';
+        };
+      };
+    }
+    (lib.mkIf config.rika.utils.hasSecrets {
+      keycloak = {
+        enable = true;
+        database.passwordFile = config.age.secrets.keycloak-db-password.path;
+        settings = {
+          http-enabled = true;
+          http-host = "127.0.0.1";
+          http-port = 8081;
+          proxy-headers = "xforwarded";
+          hostname = keycloakDomain;
+        };
+      };
+      headscale = {
+        enable = true;
+        address = "127.0.0.1";
+        settings = {
+          server_url = headscaleURI;
+          policy.mode = "database";
+          dns = {
+            magic_dns = true;
+            base_domain = "wired.local";
+            search_domains = [ ];
+            override_local_dns = false;
+            extra_records = [ ];
+            nameservers = {
+              split = { };
+              global = [
+                "1.1.1.1"
+                "1.0.0.1"
+              ];
+            };
+          };
+          oidc = {
+            issuer = oidcIssuer;
+            client_id = oidcClientId;
+            client_secret_path = config.age.secrets.headscale-oidc-client-secret.path;
+            scope = [
+              "openid"
+              "profile"
+              "email"
             ];
           };
         };
-        oidc = {
-          issuer = oidcIssuer;
-          client_id = oidcClientId;
-          client_secret_path = config.age.secrets.headscale-oidc-client-secret.path;
-          scope = [
-            "openid"
-            "profile"
-            "email"
-          ];
+      };
+      headplane = {
+        enable = true;
+        settings = {
+          integration = {
+            proc.enabled = false;
+            agent.pre_authkey_path = config.age.secrets.headscale-pre-auth-key.path;
+          };
+          server = {
+            host = "127.0.0.1";
+            cookie_secure = true;
+            cookie_secret_path = config.age.secrets.headscale-cookie-secret.path;
+          };
+          headscale = {
+            url = headscaleURI;
+            config_path = config.services.headscale.configFile;
+            config_strict = false;
+          };
+          oidc = {
+            issuer = oidcIssuer;
+            client_id = oidcClientId;
+            client_secret_path = config.age.secrets.headscale-oidc-client-secret.path;
+            headscale_api_key_path = config.age.secrets.headplane-oidc-api-key.path;
+            redirect_uri = "${headscaleURI}/admin/oidc/callback";
+          };
         };
       };
-    };
-    headplane = lib.mkIf config.rika.utils.hasSecrets {
-      enable = true;
-      settings = {
-        integration = {
-          proc.enabled = false;
-          agent.pre_authkey_path = config.age.secrets.headscale-pre-auth-key.path;
-        };
-        server = {
-          host = "127.0.0.1";
-          cookie_secure = true;
-          cookie_secret_path = config.age.secrets.headscale-cookie-secret.path;
-        };
-        headscale = {
-          url = headscaleURI;
-          config_path = config.services.headscale.configFile;
-          config_strict = false;
-        };
-        oidc = {
-          issuer = oidcIssuer;
-          client_id = oidcClientId;
-          client_secret_path = config.age.secrets.headscale-oidc-client-secret.path;
-          headscale_api_key_path = config.age.secrets.headplane-oidc-api-key.path;
-          redirect_uri = "${headscaleURI}/admin/oidc/callback";
-        };
-      };
-    };
-    openssh = {
-      enable = true;
-      openFirewall = true;
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-      };
-    };
-    caddy = {
-      enable = true;
-      openFirewall = true;
-      virtualHosts = {
-        ${headscaleDomain}.extraConfig = ''
-          handle /admin* {
-            reverse_proxy 127.0.0.1:${toString config.services.headplane.settings.server.port}
-          }
-
-          reverse_proxy 127.0.0.1:${toString config.services.headscale.port}
-        '';
-        ${keycloakDomain}.extraConfig = ''
-          reverse_proxy 127.0.0.1:${toString config.services.keycloak.settings.http-port}
-        '';
-      };
-    };
-  };
+    })
+  ];
 
   users.users.lain = {
     isNormalUser = true;
