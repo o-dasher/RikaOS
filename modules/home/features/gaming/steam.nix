7cd08630fdb28fa1de2d36d7284a760cfa57cb81
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-02-28 23:03:58 -0400

    Fix steam autostart not being the proper one that has extraCompatPackages

--- modules/home/features/gaming/steam.nix
+++ modules/home/features/gaming/steam.nix
@@ -2,17 +2,19 @@
   pkgs,
   lib,
   config,
+  osConfig ? null,
   ...
 }:
 let
   modCfg = config.features.gaming;
   cfg = modCfg.steam;
+  steamPackage = if osConfig != null then osConfig.programs.steam.package else pkgs.steam;
 in
 with lib;
 {
   options.features.gaming.steam.enable = mkEnableOption "Steam";
 
   config = mkIf (modCfg.enable && cfg.enable) {
-    xdg.configFile = config.rika.utils.mkAutostartApp pkgs.steam "${getExe pkgs.steam} -silent";
+    xdg.configFile = config.rika.utils.mkAutostartApp steamPackage "${getExe steamPackage} -silent";
   };
 }
