a3c10e15b659cf7c696591b44abecc2afcc1fe03
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-04-12 22:56:58 -0400

    Improve gamescope module

--- modules/home/features/gaming/gamescope.nix
+++ modules/home/features/gaming/gamescope.nix
@@ -6,71 +6,58 @@
 }:
 let
   cfg = config.features.gaming.gamescope;
+
+  baseArgsStr = lib.concatStringsSep " " cfg.args;
+  mkGamescopeWrapper =
+    args: pkg:
+    let
+      binName = pkg.meta.mainProgram or (lib.getName pkg);
+      argsString = lib.concatStringsSep " " (
+        lib.filter (x: x != "") [
+          baseArgsStr
+          args
+        ]
+      );
+    in
+    pkgs.symlinkJoin {
+      name = "${pkg.name}-gamescope";
+      paths = [ pkg ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        rm $out/bin/${binName}
+        makeWrapper ${lib.getExe pkgs.gamescope} $out/bin/${binName} \
+          --add-flags "${argsString} -- ${lib.getExe pkg}"
+      '';
+    };
 in
 {
   options.features.gaming.gamescope = {
     enable = lib.mkEnableOption "gamescope wrapper function";
-
     args = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [ ];
-      description = "Default gamescope arguments applied to all wrapped packages";
-      example = [
-        "-w 2560"
-        "-h 1440"
-      ];
+      description = "Default gamescope arguments.";
     };
-
     wrap = lib.mkOption {
       type = lib.types.unspecified;
       readOnly = true;
-      description = "Function to wrap a package with gamescope: wrap \"extra-args\" pkg";
     };
-
     wrapDefault = lib.mkOption {
       type = lib.types.unspecified;
       readOnly = true;
-      description = "Function to wrap a package with only global args: wrapDefault pkg";
     };
   };
 
   config = lib.mkIf (config.features.gaming.enable && cfg.enable) {
-    features.gaming.gamescope = rec {
-      wrapDefault = wrap "";
-      wrap =
-        args: pkg:
-        let
-          allArgs = lib.concatStringsSep " " (cfg.args ++ (lib.optional (args != "") args));
-          binName = pkg.meta.mainProgram or (lib.getName pkg);
-        in
-        pkgs.runCommand "${pkg.name}-gamescope"
-          {
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            meta = pkg.meta // {
-              mainProgram = binName;
-            };
-          }
-          ''
-            mkdir -p $out/bin
-            for f in ${pkg}/bin/*; do
-              ln -s "$f" "$out/bin/$(basename "$f")"
-            done
-
-            rm $out/bin/${binName}
-            makeWrapper ${lib.getExe pkgs.gamescope} $out/bin/${binName} \
-              --add-flags "${allArgs} -- ${pkg}/bin/${binName}"
-
-            # Symlink share for desktop files, icons, etc.
-            if [ -d "${pkg}/share" ]; then
-              ln -s ${pkg}/share $out/share
-            fi
-          '';
+    features.gaming.gamescope = {
+      wrap = mkGamescopeWrapper;
+      wrapDefault = mkGamescopeWrapper "";
     };
 
-    home.packages = with pkgs; [
-      gamescope
-      (writeShellScriptBin "gamescope-default" ''
-        exec ${lib.getExe gamescope} ${lib.concatStringsSep " " cfg.args} "$@"
+    home.packages = [
+      pkgs.gamescope
+      (pkgs.writeShellScriptBin "gamescope-default" ''
+        exec ${lib.getExe pkgs.gamescope} ${lib.concatStringsSep " " cfg.args} "$@"
       '')
     ];
   };
