22912aeb9f07a1c71aeaa3cc5801dffe6c020577
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-04-21 20:23:52 -0400

    Fix snippets

--- dotfiles/nvim/lua/thiago/plugins/blink_cmp.lua
+++ dotfiles/nvim/lua/thiago/plugins/blink_cmp.lua
@@ -1,34 +1,40 @@
 return {
-	"blink.cmp",
-	dep_of = { "nvim-lspconfig" },
-	after = function()
-		vim.keymap.set("i", "<C-Space>", function()
-			require("blink.cmp").show()
-		end, { silent = true })
+	{
+		"friendly-snippets",
+		dep_of = "blink.cmp",
+	},
+	{
+		"blink.cmp",
+		dep_of = { "nvim-lspconfig" },
+		after = function()
+			vim.keymap.set("i", "<C-Space>", function()
+				require("blink.cmp").show()
+			end, { silent = true })
 
-		require("blink.cmp").setup({
-			keymap = {
-				preset = "enter",
-			},
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
-			signature = { enabled = true },
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			snippets = {
-				preset = "default",
-			},
-			completion = {
-				accept = { auto_brackets = { enabled = true } },
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 250,
-					treesitter_highlighting = true,
+			require("blink.cmp").setup({
+				keymap = {
+					preset = "enter",
 				},
-			},
-		})
-	end,
+				appearance = {
+					use_nvim_cmp_as_default = true,
+					nerd_font_variant = "mono",
+				},
+				signature = { enabled = true },
+				sources = {
+					default = { "lsp", "path", "snippets", "buffer" },
+				},
+				snippets = {
+					preset = "default",
+				},
+				completion = {
+					accept = { auto_brackets = { enabled = true } },
+					documentation = {
+						auto_show = true,
+						auto_show_delay_ms = 250,
+						treesitter_highlighting = true,
+					},
+				},
+			})
+		end,
+	},
 }
