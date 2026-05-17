f7c14867a4b26db16a2f940456532dcaab1e2d13
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-05-16 23:24:52 -0400

    Fix gemini api key

--- modules/home/features/development/secrets.nix
+++ modules/home/features/development/secrets.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 let
@@ -13,10 +12,10 @@ with lib;
   options.features.development.secrets.enable = mkEnableOption "secrets";
 
   config = mkIf (modCfg.enable && cfg.enable && config.rika.utils.hasSecrets) {
-    home.sessionVariables = {
-      GEMINI_API_KEY = ''
-        $(${pkgs.coreutils}/bin/cat ${config.age.secrets.gemini-api-key.path})
-      '';
-    };
+    home.sessionVariablesExtra = ''
+      if [ -r "/run/agenix/gemini-api-key" ]; then
+        export GEMINI_API_KEY=$(cat "/run/agenix/gemini-api-key")
+      fi
+    '';
   };
 }
