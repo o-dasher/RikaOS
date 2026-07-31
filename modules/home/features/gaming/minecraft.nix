9242b27b74a7133368457eda6f3fc8f01a7c049c
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-07-31 08:16:52 -0400

    Use home manager prism launcher option instead

--- modules/home/features/gaming/minecraft.nix
+++ modules/home/features/gaming/minecraft.nix
@@ -13,15 +13,16 @@ with lib;
   options.features.gaming.minecraft.enable = mkEnableOption "minecraft";
 
   config = mkIf (modCfg.enable && cfg.enable) {
-    home.packages = [
-      (pkgs.prismlauncher.override {
+    programs.prismlauncher = {
+      enable = true;
+      package = pkgs.prismlauncher.override {
         jdks = with pkgs; [
           temurin-jre-bin-25
           temurin-jre-bin-21
           temurin-jre-bin-17
           temurin-jre-bin-8
         ];
-      })
-    ];
+      };
+    };
   };
 }
