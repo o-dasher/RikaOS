33fafdb6362c30ef0a16aac0a2e338a0efb3b10b
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-08-05 19:29:24 +0000

    fix: fix colmena

--- modules/nixos/features/core/colmena.nix
+++ modules/nixos/features/core/colmena.nix
@@ -11,12 +11,15 @@ in
   options.features.core.colmena.enable = lib.mkEnableOption "colmena deployment user";
 
   config = lib.mkIf (config.features.core.enable && cfg.enable) {
-    users.users.colmena = {
-      isSystemUser = true;
-      group = "colmena";
-      shell = pkgs.bashInteractive;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [ config.features.services.openssh.keys.rika ];
+    users = {
+      groups.colmena = { };
+      users.colmena = {
+        isSystemUser = true;
+        group = "colmena";
+        shell = pkgs.bashInteractive;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [ config.features.services.openssh.keys.rika ];
+      };
     };
 
     security.sudo.extraRules = [
