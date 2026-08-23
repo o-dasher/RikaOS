bc01eddba6960cd0d6adc084ad497062aa98ba2e
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-08-23 12:00:16 -0400

    Fix hyprlock

--- modules/home/features/desktop/hyprland/default.nix
+++ modules/home/features/desktop/hyprland/default.nix
@@ -52,7 +52,7 @@ in
             general = {
               lock_cmd = exec-sh "${lib.getExe' pkgs.procps "pidof"} hyprlock || ${lib.getExe pkgs.hyprlock}";
               before_sleep_cmd = exec "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
-              after_sleep_cmd = exec "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on";
+              after_sleep_cmd = exec "${lib.getExe' pkgs.hyprland "hyprctl"} 'dispatch(dpms, on)'";
               inhibit_sleep = 3;
             };
             listener = [
@@ -62,8 +62,8 @@ in
               }
               {
                 timeout = 600;
-                on-timeout = exec "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms off";
-                on-resume = exec "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on";
+                on-timeout = exec "${lib.getExe' pkgs.hyprland "hyprctl"} 'dispatch(dpms, off)'";
+                on-resume = exec "${lib.getExe' pkgs.hyprland "hyprctl"} 'dispatch(dpms, on)'";
               }
               {
                 timeout = 1800;
