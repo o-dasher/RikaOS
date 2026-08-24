85cda194c8eb748d06a3986ba45f729e70c8541f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-08-24 04:19:23 +0000

    feat(social/zapzap): make zapzap have less priority

--- modules/home/features/social/zapzap.nix
+++ modules/home/features/social/zapzap.nix
@@ -7,23 +7,34 @@
 let
   modCfg = config.features.social;
   cfg = modCfg.zapzap;
+
+  chromiumFlags = builtins.concatStringsSep " " [
+    "--js-flags=--max-old-space-size=2048"
+    "--disk-cache-size=${toString (128 * 1024 * 1024)}"
+    "--renderer-process-limit=1"
+    "--enable-features=IntensiveWakeUpThrottling:grace_period_seconds/10"
+  ];
+
+  zapzap-pkg = pkgs.symlinkJoin {
+    name = "zapzap";
+    paths = [
+      (pkgs.writeShellScriptBin "zapzap" ''
+        export QTWEBENGINE_CHROMIUM_FLAGS="''${QTWEBENGINE_CHROMIUM_FLAGS:-} ${chromiumFlags}"
+        exec ${lib.getExe' pkgs.coreutils "nice"} -n 19 ${lib.getExe' pkgs.util-linux "ionice"} -c 3 ${lib.getExe' pkgs.zapzap "zapzap"} "$@"
+      '')
+      pkgs.zapzap
+    ];
+  };
 in
 {
   options.features.social.zapzap.enable = lib.mkEnableOption "ZapZap";
 
   config = lib.mkIf (modCfg.enable && cfg.enable) {
-    xdg.autostart.entries = [ (config.rika.utils.mkAutostartApp { pkg = pkgs.zapzap; }) ];
-
-    # Mitigate QtWebEngine / Chromium CPU & memory accumulation over long sessions
-    home.sessionVariables.QTWEBENGINE_CHROMIUM_FLAGS = builtins.concatStringsSep " " [
-      "--js-flags=--max-old-space-size=2048"
-      "--disk-cache-size=${toString (128 * 1024 * 1024)}"
-      "--renderer-process-limit=1"
-      "--disable-background-timer-throttling=false"
-    ];
+    xdg.autostart.entries = [ (config.rika.utils.mkAutostartApp { pkg = zapzap-pkg; }) ];
 
     programs.zapzap = {
       enable = true;
+      package = zapzap-pkg;
       settings = {
         onboarding.initial_setup_completed = true;
         web.scroll_animator = false;
