82df2c29b4f66343ea1f88dfb04ff55f5b858d12
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-08-26 15:57:05 -0400

    Add common extensions for both chromium and brave

--- modules/home/profiles/browser.nix
+++ modules/home/profiles/browser.nix
@@ -11,32 +11,38 @@ in
   options.profiles.browser.enable = lib.mkEnableOption "browser profile";
 
   config = lib.mkIf cfg.enable {
-    programs = {
-      floorp = {
-        enable = true;
-        profiles.default = {
-          id = 0;
-          isDefault = true;
-          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-            darkreader
-            bitwarden
+    programs =
+      let
+        commonExtensions = [
+          "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+        ];
+      in
+      {
+        floorp = {
+          enable = true;
+          profiles.default = {
+            id = 0;
+            isDefault = true;
+            extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              darkreader
+              bitwarden
+            ];
+          };
+        };
+        brave = {
+          enable = true;
+          package = pkgs.brave-origin;
+          extensions = commonExtensions ++ [
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+            "ehdehfcjlmekjdolbbmjgokdfeoocccd" # osu! subdivide nations
           ];
         };
+        chromium = {
+          enable = true;
+          package = pkgs.ungoogled-chromium;
+          extensions = commonExtensions;
+        };
       };
-      brave = {
-        enable = true;
-        package = pkgs.brave-origin;
-        extensions = [
-          "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-          "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
-          "ehdehfcjlmekjdolbbmjgokdfeoocccd" # osu! subdivide nations
-        ];
-      };
-      chromium = {
-        enable = true;
-        package = pkgs.ungoogled-chromium;
-      };
-    };
   };
 }
