4aec8b570de4b301cb8d416ff90a1edef3858d33
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-09-11 23:20:26 -0400

    Update

--- modules/home/profiles/multimedia.nix
+++ modules/home/profiles/multimedia.nix
@@ -15,7 +15,6 @@ in
   config = lib.mkIf cfg.enable {
     services.easyeffects.enable = true;
     systemd.user.services.easyeffects = {
-      Service.Environment = [ "QT_QPA_PLATFORM=wayland;xcb;offscreen" ];
       Unit = {
         After = [ "pipewire.service" ];
         Wants = [ "pipewire.service" ];
