3ca28c897c7f049dd1934f0b7e931ba027f03b8f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-09-13 19:48:11 -0400

    feat(otd): add udev rules for wacom & lc87 firmware flashing

--- modules/nixos/features/gaming/opentabletdriver.nix
+++ modules/nixos/features/gaming/opentabletdriver.nix
@@ -8,8 +8,15 @@ let
   cfg = modCfg.otd;
 in
 {
-  options.features.gaming.otd.enable = lib.mkEnableOption "OpenTabletDriver." // {
-    default = true;
+  options.features.gaming.otd = {
+    enable = lib.mkEnableOption "OpenTabletDriver." // {
+      default = true;
+    };
+    firmwareFlashing.enable =
+      lib.mkEnableOption "udev rules for Wacom and LC87 firmware flashing (https://xstarry.dev/firmware)."
+      // {
+        default = true;
+      };
   };
 
   config = lib.mkIf (modCfg.enable && cfg.enable) {
@@ -20,5 +27,13 @@ in
         daemon.enable = true;
       };
     };
+
+    services.udev.extraRules = lib.mkIf cfg.firmwareFlashing.enable ''
+      # Wacom firmware flashing (WebHID) and SANYO LC87 bootloader (WebUSB) (https://xstarry.dev/firmware)
+      KERNEL=="hidraw*", ATTRS{idVendor}=="056a", TAG+="uaccess"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="0531", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="0ac3", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0ac3", TAG+="uaccess"
+    '';
   };
 }
