f3706fc33329d13c01ede4d88fdd9139f6d89da4
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-09-13 19:27:43 -0400

    feat(osu!): osu! sometimes fails when pipewire latency is way too low

--- modules/home/features/gaming/osu.nix
+++ modules/home/features/gaming/osu.nix
@@ -15,11 +15,7 @@ in
   config = lib.mkIf (modCfg.enable && cfg.enable) {
     home = {
       sessionVariables.OSU_SDL3 = 1;
-      packages = [
-        (inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin.override {
-          pipewire_latency = "32/48000";
-        })
-      ];
+      packages = [ inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin ];
     };
   };
 }
