24e5d8bede11409a0e9cc9f2863b9c91b9dd8c62
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-09-15 00:04:18 -0400

    refactor(services/openrgb): remove launch workarounds

--- modules/nixos/features/services/openrgb.nix
+++ modules/nixos/features/services/openrgb.nix
@@ -1,17 +1,12 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 let
   modCfg = config.features.services;
   cfg = modCfg.openrgb;
   profileName = config.services.hardware.openrgb.startupProfile;
-  openrgbReloadScript = pkgs.writeShellScript "openrgb-reload" ''
-    ${lib.getExe' pkgs.coreutils "sleep"} 3
-    ${lib.getExe pkgs.openrgb} --profile ${profileName} || true
-  '';
 in
 {
   options.features.services.openrgb.enable = lib.mkEnableOption "OpenRGB.";
@@ -22,27 +17,11 @@ in
       startupProfile = "black";
     };
 
-    systemd = {
-      # Wait for sata-based usb rgb devices.
-      services = {
-        openrgb.serviceConfig = {
-          ExecStartPre = "${lib.getExe' pkgs.coreutils "sleep"} 3";
-          ExecStartPost = lib.mkAfter [ "${openrgbReloadScript}" ];
-        };
-        systemd-suspend.serviceConfig.ExecStartPost = lib.mkAfter [ "${openrgbReloadScript}" ];
-        systemd-hibernate.serviceConfig.ExecStartPost = lib.mkAfter [ "${openrgbReloadScript}" ];
-        systemd-hybrid-sleep.serviceConfig.ExecStartPost = lib.mkAfter [ "${openrgbReloadScript}" ];
-        systemd-suspend-then-hibernate.serviceConfig.ExecStartPost = lib.mkAfter [
-          "${openrgbReloadScript}"
-        ];
-      };
-
-      tmpfiles.rules = [
-        "d /var/lib/OpenRGB/profiles 0755 root root -"
-        "L+ /var/lib/OpenRGB/Configuration.json - - - - ${../../../../assets/OpenRGB/Configuration.json}"
-        "L+ /var/lib/OpenRGB/profiles/${profileName}.json - - - - ${../../../../assets/OpenRGB/black.json}"
-        "L+ /var/lib/OpenRGB/black.orp - - - - ${../../../../assets/OpenRGB/black.orp}"
-      ];
-    };
+    systemd.tmpfiles.rules = [
+      "d /var/lib/OpenRGB/profiles 0755 root root -"
+      "L+ /var/lib/OpenRGB/Configuration.json - - - - ${../../../../assets/OpenRGB/Configuration.json}"
+      "L+ /var/lib/OpenRGB/profiles/${profileName}.json - - - - ${../../../../assets/OpenRGB/black.json}"
+      "L+ /var/lib/OpenRGB/black.orp - - - - ${../../../../assets/OpenRGB/black.orp}"
+    ];
   };
 }
